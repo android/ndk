77dc872c5c4ae67e051d1bf7edf96ce36c7b9be2
David 'Digit' Turner <digit@google.com>
2010-10-23 09:50:12 +0200

    Add stlport source module to the NDK.
    See README file to see how to use it, or build+run the unit-test program.
    
    Change-Id: Ifd09580b2ffbc3bfa268282712dbd0d9330fdb9b

---
+++ sources/android/stlport/test/jni/Android.mk
@@ -0,0 +1,27 @@
+# The source files are actually located under ../unit
+LOCAL_PATH := $(dir $(call my-dir))
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := test_stlport
+
+unit_path := $(dir $(LOCAL_PATH))/unit
+sources := $(wildcard $(unit_path)/*.cpp $(unit_path)/*.c)
+sources := $(sources:$(LOCAL_PATH)/%=%)
+
+# Both a C and a C++ source file with the same basename
+sources := $(filter-out unit/string_header_test.c,$(sources))
+
+# Exceptions are disabled for now
+sources := $(filter-out unit/exception_test.cpp,$(sources))
+
+# This test enters an infinit loop for unknown reasons!
+sources := $(filter-out unit/codecvt_test.cpp,$(sources))
+
+LOCAL_SRC_FILES := $(sources)
+LOCAL_SRC_FILES += unit/cppunit/test_main.cpp
+
+LOCAL_STATIC_LIBRARIES := stlport_static
+include $(BUILD_EXECUTABLE)
+
+$(call import-module,android/stlport)
+
