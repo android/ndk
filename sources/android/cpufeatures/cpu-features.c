7a6b7eabd10b7dc6899ac9cc08069c23d254d479
David 'Digit' Turner <digit@android.com>
2011-03-10 13:39:47 +0100

    cpu-features: Add android_getCpuCount() function.
    This new function returns the number of CPU cores detected by
    parsing /proc/cpuinfo.
    
    Also add a new ARM feature flag: ANDROID_CPU_FEATURE_ARM_LDREX_STREX
    to indicate whether the CPU support the new LDREX/STREX instructions
    introduced with ARMv6.
    
    Change-Id: I4bfd455ec517cf94bc44f946c2453a785b3d006b

--- sources/android/cpufeatures/cpu-features.c
+++ sources/android/cpufeatures/cpu-features.c
@@ -55,8 +55,17 @@
 static  pthread_once_t     g_once;
 static  AndroidCpuFamily   g_cpuFamily;
 static  uint64_t           g_cpuFeatures;
+static  int                g_cpuCount;
 
 static const int  android_cpufeatures_debug = 0;
+
+#ifdef __arm__
+#  define DEFAULT_CPU_FAMILY  ANDROID_CPU_FAMILY_ARM
+#elif defined __i386__
+#  define DEFAULT_CPU_FAMILY  ANDROID_CPU_FAMILY_X86
+#else
+#  define DEFAULT_CPU_FAMILY  ANDROID_CPU_FAMILY_UNKNOWN
+#endif
 
 #define  D(...) \
     do { \
@@ -143,6 +152,50 @@ EXIT:
     return result;
 }
 
+/* Count the number of occurences of a given field prefix in /proc/cpuinfo.
+ */
+static int
+count_cpuinfo_field(char* buffer, int buflen, const char* field)
+{
+    int fieldlen = strlen(field);
+    const char* p = buffer;
+    const char* bufend = buffer + buflen;
+    const char* q;
+    int count = 0;
+
+    for (;;) {
+        const char* q;
+
+        p = memmem(p, bufend-p, field, fieldlen);
+        if (p == NULL)
+            break;
+
+        /* Ensure that the field is at the start of a line */
+        if (p > buffer && p[-1] != '\n') {
+            p += fieldlen;
+            continue;
+        }
+
+
+        /* skip any whitespace */
+        q = p + fieldlen;
+        while (q < bufend && (*q == ' ' || *q == '\t'))
+            q++;
+
+        /* we must have a colon now */
+        if (q < bufend && *q == ':') {
+            count += 1;
+            q ++;
+        }
+        p = q;
+    }
+
+    return count;
+}
+
+/* Like strlen(), but for constant string literals */
+#define STRLEN_CONST(x)  ((sizeof(x)-1)
+
 
 /* Checks that a space-separated list of items contains one given 'item'.
  * Returns 1 if found, 0 otherwise.
@@ -181,23 +234,36 @@ has_list_item(const char* list, const char* item)
 static void
 android_cpuInit(void)
 {
-    g_cpuFamily   = ANDROID_CPU_FAMILY_UNKNOWN;
+    char cpuinfo[4096];
+    int  cpuinfo_len;
+
+    g_cpuFamily   = DEFAULT_CPU_FAMILY;
     g_cpuFeatures = 0;
+    g_cpuCount    = 1;
+
+    cpuinfo_len = read_file("/proc/cpuinfo", cpuinfo, sizeof cpuinfo);
+    D("cpuinfo_len is (%d):\n%.*s\n", cpuinfo_len,
+      cpuinfo_len >= 0 ? cpuinfo_len : 0, cpuinfo);
+
+    if (cpuinfo_len < 0)  /* should not happen */ {
+        return;
+    }
+
+    /* Count the CPU cores, the value may be 0 for single-core CPUs */
+    g_cpuCount = count_cpuinfo_field(cpuinfo, cpuinfo_len, "processor");
+    if (g_cpuCount == 0) {
+        g_cpuCount = count_cpuinfo_field(cpuinfo, cpuinfo_len, "Processor");
+        if (g_cpuCount == 0) {
+            g_cpuCount = 1;
+        }
+    }
+
+    D("found cpuCount = %d\n", g_cpuCount);
 
 #ifdef __ARM_ARCH__
     {
-        char   cpuinfo[4096];
-        int    cpuinfo_len = read_file("/proc/cpuinfo", cpuinfo, sizeof cpuinfo);
         char*  features = NULL;
         char*  architecture = NULL;
-
-        g_cpuFamily = ANDROID_CPU_FAMILY_ARM;
-
-        D("cpuinfo_len is (%d):\n%.*s\n", cpuinfo_len, cpuinfo_len, cpuinfo);
-
-        if (cpuinfo_len < 0)  /* should not happen */ {
-            return;
-        }
 
         /* Extract architecture from the "CPU Architecture" field.
          * The list is well-known, unlike the the output of
@@ -207,83 +273,84 @@ android_cpuInit(void)
          * $KERNEL/arch/arm/kernel/setup.c and the 'c_show' function in
          * same file.
          */
-        {
-            char* cpuArch = extract_cpuinfo_field(cpuinfo, cpuinfo_len, "CPU architecture");
+        char* cpuArch = extract_cpuinfo_field(cpuinfo, cpuinfo_len, "CPU architecture");
 
-            if (cpuArch != NULL) {
-                char*  end;
-                long   archNumber;
-                int    hasARMv7 = 0;
+        if (cpuArch != NULL) {
+            char*  end;
+            long   archNumber;
+            int    hasARMv7 = 0;
 
-                D("found cpuArch = '%s'\n", cpuArch);
+            D("found cpuArch = '%s'\n", cpuArch);
 
-                /* read the initial decimal number, ignore the rest */
-                archNumber = strtol(cpuArch, &end, 10);
+            /* read the initial decimal number, ignore the rest */
+            archNumber = strtol(cpuArch, &end, 10);
 
-                /* Here we assume that ARMv8 will be upwards compatible with v7
-                 * in the future. Unfortunately, there is no 'Features' field to
-                 * indicate that Thumb-2 is supported.
-                 */
-                if (end > cpuArch && archNumber >= 7) {
-                    hasARMv7 = 1;
-                }
-
-                /* Unfortunately, it seems that certain ARMv6-based CPUs
-                 * report an incorrect architecture number of 7!
-                 *
-                 * See http://code.google.com/p/android/issues/detail?id=10812
-                 *
-                 * We try to correct this by looking at the 'elf_format'
-                 * field reported by the 'Processor' field, which is of the
-                 * form of "(v7l)" for an ARMv7-based CPU, and "(v6l)" for
-                 * an ARMv6-one.
-                 */
-                if (hasARMv7) {
-                    char* cpuProc = extract_cpuinfo_field(cpuinfo, cpuinfo_len,
-                                                          "Processor");
-                    if (cpuProc != NULL) {
-                        D("found cpuProc = '%s'\n", cpuProc);
-                        if (has_list_item(cpuProc, "(v6l)")) {
-                            D("CPU processor and architecture mismatch!!\n");
-                            hasARMv7 = 0;
-                        }
-                        free(cpuProc);
-                    }
-                }
-
-                if (hasARMv7) {
-                    g_cpuFeatures |= ANDROID_CPU_ARM_FEATURE_ARMv7;
-                }
-
-                free(cpuArch);
+            /* Here we assume that ARMv8 will be upwards compatible with v7
+                * in the future. Unfortunately, there is no 'Features' field to
+                * indicate that Thumb-2 is supported.
+                */
+            if (end > cpuArch && archNumber >= 7) {
+                hasARMv7 = 1;
             }
+
+            /* Unfortunately, it seems that certain ARMv6-based CPUs
+             * report an incorrect architecture number of 7!
+             *
+             * See http://code.google.com/p/android/issues/detail?id=10812
+             *
+             * We try to correct this by looking at the 'elf_format'
+             * field reported by the 'Processor' field, which is of the
+             * form of "(v7l)" for an ARMv7-based CPU, and "(v6l)" for
+             * an ARMv6-one.
+             */
+            if (hasARMv7) {
+                char* cpuProc = extract_cpuinfo_field(cpuinfo, cpuinfo_len,
+                                                      "Processor");
+                if (cpuProc != NULL) {
+                    D("found cpuProc = '%s'\n", cpuProc);
+                    if (has_list_item(cpuProc, "(v6l)")) {
+                        D("CPU processor and architecture mismatch!!\n");
+                        hasARMv7 = 0;
+                    }
+                    free(cpuProc);
+                }
+            }
+
+            if (hasARMv7) {
+                g_cpuFeatures |= ANDROID_CPU_ARM_FEATURE_ARMv7;
+            }
+
+            /* The LDREX / STREX instructions are available from ARMv6 */
+            if (archNumber >= 6) {
+                g_cpuFeatures |= ANDROID_CPU_ARM_FEATURE_LDREX_STREX;
+            }
+
+            free(cpuArch);
         }
 
         /* Extract the list of CPU features from 'Features' field */
-        {
-            char* cpuFeatures = extract_cpuinfo_field(cpuinfo, cpuinfo_len, "Features");
+        char* cpuFeatures = extract_cpuinfo_field(cpuinfo, cpuinfo_len, "Features");
 
-            if (cpuFeatures != NULL) {
+        if (cpuFeatures != NULL) {
 
-                D("found cpuFeatures = '%s'\n", cpuFeatures);
+            D("found cpuFeatures = '%s'\n", cpuFeatures);
 
-                if (has_list_item(cpuFeatures, "vfpv3"))
-                    g_cpuFeatures |= ANDROID_CPU_ARM_FEATURE_VFPv3;
+            if (has_list_item(cpuFeatures, "vfpv3"))
+                g_cpuFeatures |= ANDROID_CPU_ARM_FEATURE_VFPv3;
 
-                else if (has_list_item(cpuFeatures, "vfpv3d16"))
-                    g_cpuFeatures |= ANDROID_CPU_ARM_FEATURE_VFPv3;
+            else if (has_list_item(cpuFeatures, "vfpv3d16"))
+                g_cpuFeatures |= ANDROID_CPU_ARM_FEATURE_VFPv3;
 
-                if (has_list_item(cpuFeatures, "neon")) {
-                    /* Note: Certain kernels only report neon but not vfpv3
-                     *       in their features list. However, ARM mandates
-                     *       that if Neon is implemented, so must be VFPv3
-                     *       so always set the flag.
-                     */
-                    g_cpuFeatures |= ANDROID_CPU_ARM_FEATURE_NEON |
-                                     ANDROID_CPU_ARM_FEATURE_VFPv3;
-                }
-                free(cpuFeatures);
+            if (has_list_item(cpuFeatures, "neon")) {
+                /* Note: Certain kernels only report neon but not vfpv3
+                    *       in their features list. However, ARM mandates
+                    *       that if Neon is implemented, so must be VFPv3
+                    *       so always set the flag.
+                    */
+                g_cpuFeatures |= ANDROID_CPU_ARM_FEATURE_NEON |
+                                 ANDROID_CPU_ARM_FEATURE_VFPv3;
             }
+            free(cpuFeatures);
         }
     }
 #endif /* __ARM_ARCH__ */
@@ -307,4 +374,12 @@ android_getCpuFeatures(void)
 {
     pthread_once(&g_once, android_cpuInit);
     return g_cpuFeatures;
+}
+
+
+int
+android_getCpuCount(void)
+{
+    pthread_once(&g_once, android_cpuInit);
+    return g_cpuCount;
 }
