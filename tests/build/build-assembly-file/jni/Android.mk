3e55631d11957e7cd5f7d4141beb7cfaa0634fbb
Mark D Horn <mark.d.horn@intel.com>
2011-04-26 16:43:53 -0700

    Assembly test updated for x86
    Added x86 specific assembly source and updated makefile
    to select the appropriate targets.
    
    Change-Id: I390daa89f5cc7fb0b00f66f84e39b9ccf88b722a
    Signed-off-by: Mark D Horn <mark.d.horn@intel.com>

--- tests/build/build-assembly-file/jni/Android.mk
+++ tests/build/build-assembly-file/jni/Android.mk
@@ -2,5 +2,11 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := test-build-assembly
-LOCAL_SRC_FILES := assembly1.s assembly2.S
+ifeq ($(TARGET_ARCH),arm)
+    LOCAL_SRC_FILES := assembly1.s assembly2.S
+else
+    ifneq ($(TARGET_ARCH),x86)
+        LOCAL_SRC_FILES := assembly-x86.S
+    endif
+endif
 include $(BUILD_SHARED_LIBRARY)
