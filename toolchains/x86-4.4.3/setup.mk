dc4220adcd9707d66d5874c747016648139a58bb
Bruce Beare <bruce.j.beare@intel.com>
2011-05-04 10:11:32 -0700

    x86: Enable -fstack-protector
    Change-Id: Idee998212cec3c2882e7805609948eb65c7ce811
    Signed-off-by: Bruce Beare <bruce.j.beare@intel.com>

--- toolchains/x86-4.4.3/setup.mk
+++ toolchains/x86-4.4.3/setup.mk
@@ -39,8 +39,7 @@ TARGET_CFLAGS := \
 # Add and LDFLAGS for the target here
 # TARGET_LDFLAGS :=
 
-# Fix this after ssp.c is fixed for x86
-# TARGET_CFLAGS += -fstack-protector
+TARGET_CFLAGS += -fstack-protector
 
 TARGET_x86_release_CFLAGS :=  -O2 \
                               -fomit-frame-pointer \
