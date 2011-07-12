1099649d3501334235a8a39b7ff6289545695f00
Jeff Brown <jeffbrown@google.com>
2011-07-11 22:12:26 -0700

    Remove the simulator target from all makefiles. Bug: 5010576
    Change-Id: I79323a4872ebe5698dc3a97fba194f1d88eb152d

--- Android.mk
+++ Android.mk
@@ -1,6 +1,4 @@
 # Please try to keep this file empty by only including what is necessary to
 # build the Android platform. This is not the normal usage of the NDK.
 
-ifneq ($(TARGET_SIMULATOR),true)
 include ndk/sources/android/cpufeatures/Android.mk
-endif
