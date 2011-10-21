032704671f009c586ff8b75e34d5027ceab3d510
David 'Digit' Turner <digit@google.com>
2011-10-21 16:02:15 +0200

    stlport: remove on static initializer from src/monetary.cpp
    Change-Id: I1272bd9a28f25ac4b3c14320a316111fa0462daf

--- sources/cxx-stl/stlport/src/monetary.cpp
+++ sources/cxx-stl/stlport/src/monetary.cpp
@@ -36,9 +36,9 @@ static void _Init_monetary_formats(money_base::pattern& pos_format,
 }
 
 // This is being used throughout the library
-static const string _S_empty_string;
+static const char _S_empty_string[] = "";
 #ifndef _STLP_NO_WCHAR_T
-static const wstring _S_empty_wstring;
+static const wchar_t _S_empty_wstring[] = L"";
 #endif
 
 //
