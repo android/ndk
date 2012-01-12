0e2ba9c31a1cf0a7f1e5b9adfaaf276505fda3f0
David 'Digit' Turner <digit@google.com>
2012-01-12 11:52:12 +0100

    Fix gnustl-shared-1 unit test.
    The tests didn't work (crashed at runtime) because we didn't use
    -frtti in our APP_CPPFLAGS. Turns out that we can make it work
    with a 'catch (...) { }' clause, which doesn't uses RTTI at all.
    
    Change-Id: Ie70104e732cd581dd4228c36867615b52b71c08b

--- tests/device/gnustl-shared-1/jni/Application.mk
+++ tests/device/gnustl-shared-1/jni/Application.mk
@@ -1,3 +1,4 @@
 APP_STL := gnustl_shared
 APP_ABI := all
+
 APP_CPPFLAGS := -fexceptions
--- tests/device/gnustl-shared-1/jni/main.cpp
+++ tests/device/gnustl-shared-1/jni/main.cpp
@@ -8,7 +8,10 @@ int main(void)
     try {
         (void) foo();
     }
-    catch (std::exception e) {
+    // Catch all exceptions. Note that if we used catch (std::exception& e)
+    // instead, we would need to activate rtti as well to avoid crashing
+    // in the C++ runtime.
+    catch (...) {
         ::printf(" World!\n");
     }
     return 0;
