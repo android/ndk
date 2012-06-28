bc7980104475ae4ec56fddf053fa7775985229a9
Andrew Hsieh <andrewhsieh@google.com>
2012-06-27 17:52:31 -0700

    Fix ndk-build.cmd to return non-zero ERRORLEVEL when make fails
    It appears that "make" doesnʼt set ERRORLEVEL running ndk-build.cmd
    in cmd.exe shell, which causes ERRORLEVEL to remain zero even when
    make fails.  Fixed it to return non-zero value when fails.
    
    Related issue:
      http://code.google.com/p/android/issues/detail?id=32300
    
    Change-Id: Iedd6c4897035e35910ca12ef6f70730800881aba

--- ndk-build.cmd
+++ ndk-build.cmd
@@ -2,4 +2,4 @@
 rem This is a Windows cmd.exe script used to invoke the NDK-specific GNU Make executable
 set NDK_ROOT=%~dp0
 set NDK_MAKE=%NDK_ROOT%/prebuilt/windows/bin/make.exe
-%NDK_ROOT%\prebuilt\windows\bin\make.exe -f %NDK_ROOT%build/core/build-local.mk SHELL=cmd %* || exit /b %ERRORLEVEL%
+%NDK_ROOT%\prebuilt\windows\bin\make.exe -f %NDK_ROOT%build/core/build-local.mk SHELL=cmd %* || exit /b 1
