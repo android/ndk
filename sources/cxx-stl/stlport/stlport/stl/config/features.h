a900181ed5f6164cf126e7a66a754a1fc1ccf31a
Andrew Hsieh <andrewhsieh@google.com>
2012-06-29 10:45:02 -0700

    Set correct _STLP_LITTLE_ENDIAN for MIPSel
    Set _STLP_LITTLE_ENDIAN to 1 when compiling for MIPS little-endian
    Related CL: https://android-review.googlesource.com/#/c/32072/
    
    Change-Id: I9de3605dff4235bbceb22d11fc5914c4264b9631

--- sources/cxx-stl/stlport/stlport/stl/config/features.h
+++ sources/cxx-stl/stlport/stlport/stl/config/features.h
@@ -162,7 +162,7 @@
 #    define _STLP_BIG_ENDIAN 1
 #  elif defined (__i386) || defined (_M_IX86) || defined (_M_ARM) || \
         defined (__amd64__) || defined (_M_AMD64) || defined (__x86_64__) || \
-        defined (__alpha__)
+        defined (__alpha__) || defined (_MIPSEL)
 #    define _STLP_LITTLE_ENDIAN 1
 #  elif defined (__ia64__)
     /* itanium allows both settings (for instance via gcc -mbig-endian) - hence a seperate check is required */
