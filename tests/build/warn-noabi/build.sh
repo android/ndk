2aec55d96c5c3ef2d0cca58fe80cc67273cb933e
Andrew Hsieh <andrewhsieh@google.com>
2012-08-29 22:07:00 +0800

    Add test for fix in ARM g++ warning about mangling of <va_list>
    For https://android-review.googlesource.com/#/c/42274
    
    Change-Id: Iab0d149882d6b0846e9c9d068e325a96d2f405f3

---
+++ tests/build/warn-noabi/build.sh
@@ -0,0 +1,21 @@
+# Check if ARM g++ no longer gives pointless warning about the mangling of <va_list> has changed in GCC 4.4
+# See https://android-review.googlesource.com/#/c/42274/
+#
+
+NDK_BUILDTOOLS_PATH=$NDK/build/tools
+. $NDK/build/tools/prebuilt-common.sh
+
+SYSTEM=$(get_prebuilt_host_tag)
+
+ARM_GPP=$NDK/toolchains/arm-linux-androideabi-4.6/prebuilt/$SYSTEM/bin/arm-linux-androideabi-g++
+
+OUT=$(echo "#include <stdarg.h>
+void foo(va_list v) { }" | $ARM_GPP -x c++ -c -o /dev/null - 2>&1)
+
+if [ -z "$OUT" ]; then
+  echo "ARM g++ no longer gives pointless warning about the mangling of <va_list> has changed in GCC 4.4"
+  exit 0
+else
+  echo "ERROR: ARM g++ still gives pointless warning about the mangling of <va_list> has changed in GCC 4.4"
+  exit 1
+fi  
