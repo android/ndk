fa1592c047b02e401783933a39b714f679826d68
Andrew Hsieh <andrewhsieh@google.com>
2012-09-25 13:27:56 -0700

    New script run-tests-all.sh
    run-tests-all.sh calls run-tests.sh and standalone/run.sh with
    various conditions
    
    Change-Id: Ia6b35da3c06c3af4af6d06eb0996704c931aabd4

--- build/tools/dev-defaults.sh
+++ build/tools/dev-defaults.sh
@@ -61,6 +61,9 @@ DEFAULT_ARCH_TOOLCHAIN_PREFIX_mips=mipsel-linux-android
 # The space-separated list of all LLVM versions we support in NDK
 DEFAULT_LLVM_VERSION_LIST="3.1"
 
+# The default LLVM version (first item in the list)
+DEFAULT_LLVM_VERSION=$(echo "$DEFAULT_LLVM_VERSION_LIST" | tr ' ' '\n' | head -n 1)
+
 # The default URL to download the LLVM tar archive
 DEFAULT_LLVM_URL="http://llvm.org/releases"
 
