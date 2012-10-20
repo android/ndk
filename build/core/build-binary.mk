android/ndk

commit 7a06c124be094134d81920a8f9fe363f6f30abd1

Build modules with hundreds or thousands of sources.
This patch adds the support for LOCAL_SHORT_COMMANDS in Android.mk
and APP_SHORT_COMMANDS in Application.mk. Setting these variable to
'true' will force the use of a linker/archiver list file when building
your NDK modules.

This is mainly useful when you have a very large number of source
files and/or libraries on Windows, which has a limited
command-line length of 8191 characters.

Fixes http://code.google.com/p/android/issues/detail?id=25359

Change-Id: I6f4824e91231e92c0aa83631b89c462ff25ed1cc

build/core/build-binary.mk
@@ -277,10 +277,61 @@ ifneq ($(filter -l%,$(LOCAL_LDLIBS)),)
     LOCAL_LDLIBS := -L$(call host-path,$(SYSROOT)/usr/lib) $(LOCAL_LDLIBS)
 endif
 
+# The list of object/static/shared libraries passed to the linker when
+# building shared libraries and executables. order is important.
+#
+linker_objects_and_libraries := $(strip $(call TARGET-get-linker-objects-and-libraries,\
+    $(LOCAL_OBJECTS), \
+    $(static_libraries), \
+    $(whole_static_libraries), \
+    $(shared_libraries)))
+
+# The list of object files sent to 'ar' when building static libraries
+#
+ar_objects := $(call host-path,$(LOCAL_OBJECTS))
+
+# When LOCAL_SHORT_COMMANDS is defined to 'true' we are going to write the
+# list of all object files and/or static/shared libraries that appear on the
+# command line to a file, then use the @<listfile> syntax to invoke it.
+#
+# This allows us to link or archive a huge number of stuff even on Windows
+# with its puny 8192 max character limit on its command-line.
+#
+LOCAL_SHORT_COMMANDS := $(strip $(LOCAL_SHORT_COMMANDS))
+ifndef LOCAL_SHORT_COMMANDS
+    LOCAL_SHORT_COMMANDS := $(strip $(NDK_APP_SHORT_COMMANDS))
+endif
+ifeq ($(LOCAL_SHORT_COMMANDS)),true)
+    # For static and whole static libraries
+    ifneq (,$(filter STATIC_LIBRARY WHOLE_STATIC_LIBRARY,$(call module-get-class,$(LOCAL_MODULE))))
+        $(call ndk_log,Building static library module '$(LOCAL_MODULE)' with linker list file)
+        ar_options   := $(ar_objects)
+        ar_list_file := $(LOCAL_OBJS_DIR)/archiver.list
+        ar_options   := @$(call host-path,$(ar_list_file)))
+        $(call generate-list-file,$(ar_options),$(ar_list_file))
+
+        $(LOCAL_BUILT_MODULE): $(ar_list_file)
+    endif
+
+    # For shared libraries and executables
+    ifneq (,$(filter SHARED_LIBRARY EXECUTABLE,$(call module-get-class,$(LOCAL_MODULE))))
+        $(call ndk_log,Building ELF binary module '$(LOCAL_MODULE)' with linker list file)
+        linker_options   := $(linker_objects_and_libraries)
+        linker_list_file := $(LOCAL_OBJS_DIR)/linker.list
+        linker_objects_and_libraries := @$(call host-path,$(linker_list_file))
+
+        $(call generate-list-file,$(linker_options),$(linker_list_file))
+
+        $(LOCAL_BUILT_MODULE): $(linker_list_file)
+    endif
+
+endif
+
 $(LOCAL_BUILT_MODULE): PRIVATE_STATIC_LIBRARIES := $(static_libraries)
 $(LOCAL_BUILT_MODULE): PRIVATE_WHOLE_STATIC_LIBRARIES := $(whole_static_libraries)
 $(LOCAL_BUILT_MODULE): PRIVATE_SHARED_LIBRARIES := $(shared_libraries)
 $(LOCAL_BUILT_MODULE): PRIVATE_OBJECTS          := $(LOCAL_OBJECTS)
+$(LOCAL_BUILT_MODULE): PRIVATE_LINKER_OBJECTS_AND_LIBRARIES := $(linker_objects_and_libraries)
 $(LOCAL_BUILT_MODULE): PRIVATE_LIBGCC := $(TARGET_LIBGCC)
 
 $(LOCAL_BUILT_MODULE): PRIVATE_LD := $(TARGET_LD)
@@ -291,6 +342,7 @@ $(LOCAL_BUILT_MODULE): PRIVATE_NAME := $(notdir $(LOCAL_BUILT_MODULE))
 $(LOCAL_BUILT_MODULE): PRIVATE_CXX := $(TARGET_CXX)
 $(LOCAL_BUILT_MODULE): PRIVATE_CC := $(TARGET_CC)
 $(LOCAL_BUILT_MODULE): PRIVATE_AR := $(TARGET_AR) $(TARGET_ARFLAGS)
+$(LOCAL_BUILT_MODULE): PRIVATE_AR_OBJECTS := $(ar_objects)
 $(LOCAL_BUILT_MODULE): PRIVATE_SYSROOT := $(SYSROOT)
 
 #

build/core/default-build-commands.mk
@@ -8,6 +8,31 @@
 # flags.
 TARGET_NO_UNDEFINED_LDFLAGS := -Wl,--no-undefined
 
+
+# Return the list of object, static libraries and shared libraries as they
+# must appear on the final static linker command (order is important).
+#
+# This can be over-ridden by a specific toolchain. Note that by default
+# we always put libgcc _after_ all static libraries and _before_ shared
+# libraries. This ensures that any libgcc function used by the final
+# executable will be copied into it. Otherwise, it could contain
+# symbol references to the same symbols as exported by shared libraries
+# and this causes binary compatibility problems when they come from
+# system libraries (e.g. libc.so and others).
+#
+# IMPORTANT: The result must use the host path convention.
+#
+# $1: object files
+# $2: static libraries
+# $3: whole static libraries
+# $4: shared libraries
+#
+TARGET-get-linker-objects-and-libraries = \
+    $(call host-path, $1) \
+    $(call link-whole-archives,$3) \
+    $(call host-path, $2 $(PRIVATE_LIBGCC) $4) \
+
+
 # These flags are used to enfore the NX (no execute) security feature in the
 # generated machine code. This adds a special section to the generated shared
 # libraries that instruct the Linux kernel to disable code execution from
@@ -30,12 +55,7 @@ $(PRIVATE_CXX) \
     -Wl,-shared,-Bsymbolic \
     $(call host-path,\
         $(TARGET_CRTBEGIN_SO_O) \
-        $(PRIVATE_OBJECTS)) \
-    $(call link-whole-archives,$(PRIVATE_WHOLE_STATIC_LIBRARIES))\
-    $(call host-path,\
-        $(PRIVATE_STATIC_LIBRARIES) \
-        $(PRIVATE_LIBGCC) \
-        $(PRIVATE_SHARED_LIBRARIES)) \
+    $(PRIVATE_LINKER_OBJECTS_AND_LIBRARIES) \
     $(PRIVATE_LDFLAGS) \
     $(PRIVATE_LDLIBS) \
     $(call host-path,\
@@ -51,12 +71,7 @@ $(PRIVATE_CXX) \
     -Wl,-z,nocopyreloc \
     $(call host-path,\
         $(TARGET_CRTBEGIN_DYNAMIC_O) \
-        $(PRIVATE_OBJECTS)) \
-    $(call link-whole-archives,$(PRIVATE_WHOLE_STATIC_LIBRARIES))\
-    $(call host-path,\
-        $(PRIVATE_STATIC_LIBRARIES) \
-        $(PRIVATE_LIBGCC) \
-        $(PRIVATE_SHARED_LIBRARIES)) \
+    $(PRIVATE_LINKER_OBJECTS_AND_LIBRARIES) \
     $(PRIVATE_LDFLAGS) \
     $(PRIVATE_LDLIBS) \
     $(call host-path,\
@@ -65,7 +80,7 @@ $(PRIVATE_CXX) \
 endef
 
 define cmd-build-static-library
-$(PRIVATE_AR) $(call host-path,$@) $(call host-path,$(PRIVATE_OBJECTS))
+$(PRIVATE_AR) $(call host-path,$@) $(PRIVATE_AR_OBJECTS)
 endef
 
 # The strip command is only used for shared libraries and executables.

build/core/definitions.mk
@@ -241,6 +241,67 @@ endif
 
 
 # -----------------------------------------------------------------------------
+# Function : generate-list-file
+# Arguments: 1: list of strings (possibly very long)
+#            2: file name
+# Returns  : write the content of a possibly very long string list to a file.
+#            this shall be used in commands and will work around limitations
+#            of host command-line lengths.
+# Usage    : $(call host-echo-to-file,<string-list>,<file>)
+# Rationale: When there is a very large number of objects and/or libraries at
+#            link time, the size of the command becomes too large for the
+#            host system's maximum. Various tools however support the
+#            @<listfile> syntax, where <listfile> is the path of a file
+#            which content will be parsed as if they were options.
+#
+#            This function is used to generate such a list file from a long
+#            list of strings in input.
+#
+# Note: The implementation writes 100 strings per line in the target file.
+#       The smallest max command-line line length is on Windows (8191 chars),
+#       so if every string is smaller than 81 chars in the input, everything
+#       will work properly.
+#
+# -----------------------------------------------------------------------------
+
+define generate-list-file-slice
+ifneq (,$$(word $1,$3))
+	$$(hide) $$(HOST_ECHO) "$$(wordlist $1,$2,$3)" >> $$@
+endif
+endef
+
+define generate-list-file-ev
+__list_file := $2
+
+.PHONY: $$(__list_file)
+
+$$(__list_file):
+	@ $$(host-mkdir) $$(dir $$@)
+	$$(hide) $$(HOST_ECHO) "$(wordlist 1,99,$1)" > $$@
+$(call generate-list-file-slice,100,199,$1)
+$(call generate-list-file-slice,200,299,$1)
+$(call generate-list-file-slice,300,399,$1)
+$(call generate-list-file-slice,400,499,$1)
+$(call generate-list-file-slice,500,599,$1)
+$(call generate-list-file-slice,600,699,$1)
+$(call generate-list-file-slice,700,799,$1)
+$(call generate-list-file-slice,800,899,$1)
+$(call generate-list-file-slice,900,999,$1)
+$(call generate-list-file-slice,1000,1099,$1)
+$(call generate-list-file-slice,1100,1199,$1)
+$(call generate-list-file-slice,1200,1299,$1)
+$(call generate-list-file-slice,1300,1399,$1)
+$(call generate-list-file-slice,1400,1499,$1)
+$(call generate-list-file-slice,1500,1599,$1)
+$(call generate-list-file-slice,1600,1699,$1)
+$(call generate-list-file-slice,1700,1799,$1)
+$(call generate-list-file-slice,1800,1899,$1)
+$(call generate-list-file-slice,1900,1999,$1)
+endef
+
+generate-list-file = $(eval $(call generate-list-file-ev,$1,$2))
+
+# -----------------------------------------------------------------------------
 # Function : link-whole-archives
 # Arguments: 1: list of whole static libraries
 # Returns  : linker flags to use the whole static libraries
@@ -315,6 +376,7 @@ modules-LOCALS := \
     EXPORT_C_INCLUDES \
     FILTER_ASM \
     CPP_FEATURES \
+    SHORT_COMMANDS \
 
 # The following are generated by the build scripts themselves
 
@@ -1154,6 +1216,14 @@ $$(_OBJ): PRIVATE_MODULE   := $$(LOCAL_MODULE)
 $$(_OBJ): PRIVATE_TEXT     := "$$(_TEXT)"
 $$(_OBJ): PRIVATE_CC       := $$(_CC)
 $$(_OBJ): PRIVATE_CFLAGS   := $$(_FLAGS)
+
+ifeq ($$(LOCAL_SHORT_COMMANDS),true)
+_OPTIONS_LISTFILE := $$(_OBJ).cflags
+$$(_OBJ): $$(call generate-list-file,$$(_FLAGS),$$(_OPTIONS_LISTFILE))
+$$(_OBJ): PRIVATE_CFLAGS := @$$(call host-path,$$(_OPTIONS_LISTFILE))
+$$(_OBJ): $$(_OPTIONS_LISTFILE)
+endif
+
 $$(_OBJ): $$(_SRC) $$(LOCAL_MAKEFILE) $$(NDK_APP_APPLICATION_MK) $$(NDK_DEPENDENCIES_CONVERTER)
 	@$$(call host-mkdir,$$(dir $$(PRIVATE_OBJ)))
 	@$$(HOST_ECHO) "$$(PRIVATE_TEXT)  : $$(PRIVATE_MODULE) <= $$(notdir $$(PRIVATE_SRC))"

docs/ANDROID-MK.html
@@ -685,6 +685,27 @@ LOCAL_EXPORT_LDLIBS
     command to indicate that it depends on the system logging library,
     because it depends on 'foo'.
 
+LOCAL_SHORT_COMMANDS
+    Set this variable to 'true' when your module has a very high number of
+    sources and/or dependent static or shared libraries. This forces the
+    build system to use an intermediate list file, and use it with the
+    library archiver or static linker with the @$(listfile) syntax.
+
+    This can be useful on Windows, where the command-line only accepts
+    a maximum of 8191 characters, which can be too small for complex
+    projects.
+
+    This also impacts the compilation of individual source files, placing
+    nearly all compiler flags inside list files too.
+
+    Note that any other value than 'true' will revert to the default
+    behaviour. You can also define APP_SHORT_COMMANDS in your
+    Application.mk to force this behaviour for all modules in your
+    project.
+
+    NOTE: We do not recommend enabling this feature by default, since it
+          makes the build slower.
+
 LOCAL_FILTER_ASM
     Define this variable to a shell command that will be used to filter
     the assembly files from, or generated from, your LOCAL_SRC_FILES.

docs/APPLICATION-MK.html
@@ -202,6 +202,10 @@ APP_GNUSTL_FORCE_CPP_FEATURES
                developers to modify the module definitions properly instead
                of relying on it here.
 
+APP_SHORT_COMMANDS
+    The equivalent of LOCAL_SHORT_COMMANDS for your whole project. See the
+    documentation for this variable in docs/ANDROID-MK.html.
+
 A trivial Application.mk file would be:
 
 -------------- cut here -------------------------

tests/build/short-commands/build.sh
@@ -1,0 +1,157 @@
+# Special script used to check that LOCAL_SHORT_COMMANDS works
+# correctly even when using a very large number of source files
+# when building a static or shared library.
+#
+# We're going to auto-generate all the files we need in a
+# temporary directory, because that's how we roll.
+#
+
+PROGDIR=$(dirname $0)
+PROGDIR=$(cd "$PROGDIR" && pwd)
+
+# TODO: Increment this to 1000 for long test runs. For the simple unit test
+#        suite, keep this just over 100
+#
+COUNT=110
+
+# Generate all our source files
+NUM=0
+
+SRCDIR=$PROGDIR/jni
+rm -rf "$SRCDIR" && mkdir -p "$SRCDIR"
+if [ $? != 0 ]; then
+    echo "ERROR: Could not create temporary source directory: $SRCDIR"
+    exit 1
+fi
+
+CLEAN_FILES=
+
+generate_source_files ()
+{
+    # Generate all temporary source files we need
+    local NUM=0
+    while [ $NUM -lt $COUNT ]; do
+        SRCFILE=$SRCDIR/foo$NUM.c
+    cat > $SRCFILE <<EOF
+int foo$NUM (int x)
+{
+    return x + 1;
+}
+EOF
+        NUM=$(( $NUM + 1 ))
+        CLEAN_FILES=$CLEAN_FILES" $SRCFILE"
+    done
+}
+
+generate_main_file ()
+{
+    cat > $SRCDIR/main.c <<EOF
+#include <stdio.h>
+EOF
+    NUM=0
+    while [ $NUM -lt $COUNT ]; do
+        cat >> $SRCDIR/main.c <<EOF
+extern int foo$NUM (int);
+EOF
+        NUM=$(( $NUM + 1 ))
+    done
+    cat >> $SRCDIR/main.c <<EOF
+int main(void)
+{
+    int x = 0;
+EOF
+    NUM=0
+    while [ $NUM -lt $COUNT ]; do
+        cat >> $SRCDIR/main.c <<EOF
+    x = foo$NUM(x);
+EOF
+        NUM=$(( $NUM + 1 ))
+    done
+cat >> $SRCDIR/main.c <<EOF
+    return 0;
+}
+EOF
+    CLEAN_FILES=$CLEAN_FILES" $SRCDIR/main.c"
+}
+
+generate_build_file ()
+{
+    local NUM
+
+    # Generate the Android.mk
+    cat > $SRCDIR/Android.mk <<EOF
+# Auto-generated - do not edit
+LOCAL_PATH := \$(call my-dir)
+EOF
+
+    # First, build a huge static library with all the files
+    cat >> $SRCDIR/Android.mk <<EOF
+include \$(CLEAR_VARS)
+LOCAL_MODULE := libfoo_big
+EOF
+
+    NUM=0
+    while [ $NUM -lt $COUNT ]; do
+        cat >> $SRCDIR/Android.mk <<EOF
+LOCAL_SRC_FILES += foo$NUM.c
+EOF
+        NUM=$(( $NUM + 1 ))
+    done
+
+    cat >> $SRCDIR/Android.mk <<EOF
+LOCAL_SHORT_COMMANDS := true
+include \$(BUILD_SHARED_LIBRARY)
+EOF
+
+    # Second, generate a large number of static libraries
+    # Then an executable that use them all
+    NUM=0
+    while [ $NUM -lt $COUNT ]; do
+        cat >> $SRCDIR/Android.mk <<EOF
+include \$(CLEAR_VARS)
+LOCAL_MODULE := foo$NUM
+LOCAL_SRC_FILES := foo$NUM.c
+include \$(BUILD_STATIC_LIBRARY)
+EOF
+        NUM=$(( $NUM + 1 ))
+    done
+
+        cat >> $SRCDIR/Android.mk <<EOF
+include \$(CLEAR_VARS)
+LOCAL_MODULE := test_linker_options_list
+LOCAL_SRC_FILES := main.c
+EOF
+
+    NUM=0
+    while [ $NUM -lt $COUNT ]; do
+        cat >> $SRCDIR/Android.mk <<EOF
+LOCAL_WHOLE_STATIC_LIBRARIES += foo$NUM
+EOF
+        NUM=$(( $NUM + 1 ))
+    done
+
+    cat >> $SRCDIR/Android.mk <<EOF
+#LOCAL_SHORT_COMMANDS := true
+include \$(BUILD_EXECUTABLE)
+EOF
+    CLEAN_FILES=$CLEAN_FILES" $SRCDIR/Android.mk"
+}
+
+generate_source_files && 
+generate_main_file &&
+generate_build_file
+if [ $? != 0 ]; then
+    echo "ERROR: Could not generate files for this test!"
+    exit 1
+fi
+
+# Now run the build
+$NDK/ndk-build -C "$PROGDIR" "$@"
+RET=$?
+
+# Clean everything we generated
+rm -f $CLEAN_FILES
+rm -rf "$PROGDIR/obj" "$PROGDIR/libs"
+
+# Exit
+exit $RET

toolchains/arm-linux-androideabi-4.4.3/setup.mk
@@ -122,12 +122,7 @@ $(PRIVATE_CXX) \
     -Wl,-soname,$(notdir $@) \
     -shared \
     --sysroot=$(call host-path,$(PRIVATE_SYSROOT)) \
-    $(call host-path, $(PRIVATE_OBJECTS)) \
-    $(call link-whole-archives,$(PRIVATE_WHOLE_STATIC_LIBRARIES)) \
-    $(call host-path,\
-        $(PRIVATE_STATIC_LIBRARIES) \
-        $(PRIVATE_LIBGCC) \
-        $(PRIVATE_SHARED_LIBRARIES)) \
+    $(PRIVATE_LINKER_OBJECTS_AND_LIBRARIES) \
     $(PRIVATE_LDFLAGS) \
     $(PRIVATE_LDLIBS) \
     -o $(call host-path,$@)
@@ -138,12 +133,7 @@ $(PRIVATE_CXX) \
     -Wl,--gc-sections \
     -Wl,-z,nocopyreloc \
     --sysroot=$(call host-path,$(PRIVATE_SYSROOT)) \
-    $(call host-path, $(PRIVATE_OBJECTS)) \
-    $(call link-whole-archives,$(PRIVATE_WHOLE_STATIC_LIBRARIES)) \
-    $(call host-path,\
-        $(PRIVATE_STATIC_LIBRARIES) \
-        $(PRIVATE_LIBGCC) \
-        $(PRIVATE_SHARED_LIBRARIES)) \
+    $(PRIVATE_LINKER_OBJECTS_AND_LIBRARIES) \
     $(PRIVATE_LDFLAGS) \
     $(PRIVATE_LDLIBS) \
     -o $(call host-path,$@)

toolchains/x86-4.4.3/setup.mk
@@ -78,12 +78,7 @@ $(PRIVATE_CXX) \
     -Wl,-soname,$(notdir $@) \
     -shared \
     --sysroot=$(call host-path,$(PRIVATE_SYSROOT)) \
-    $(call host-path, $(PRIVATE_OBJECTS)) \
-    $(call link-whole-archives,$(PRIVATE_WHOLE_STATIC_LIBRARIES)) \
-    $(call host-path,\
-        $(PRIVATE_STATIC_LIBRARIES) \
-        $(PRIVATE_LIBGCC) \
-        $(PRIVATE_SHARED_LIBRARIES)) \
+    $(PRIVATE_LINKER_OBJECTS_AND_LIBRARIES) \
     $(PRIVATE_LDFLAGS) \
     $(PRIVATE_LDLIBS) \
     -o $(call host-path,$@)
@@ -94,12 +89,7 @@ $(PRIVATE_CXX) \
     -Wl,--gc-sections \
     -Wl,-z,nocopyreloc \
     --sysroot=$(call host-path,$(PRIVATE_SYSROOT)) \
-    $(call host-path, $(PRIVATE_OBJECTS)) \
-    $(call link-whole-archives,$(PRIVATE_WHOLE_STATIC_LIBRARIES)) \
-    $(call host-path,\
-        $(PRIVATE_STATIC_LIBRARIES) \
-        $(PRIVATE_LIBGCC) \
-        $(PRIVATE_SHARED_LIBRARIES)) \
+    $(PRIVATE_LINKER_OBJECTS_AND_LIBRARIES) \
     $(PRIVATE_LDFLAGS) \
     $(PRIVATE_LDLIBS) \
     -o $(call host-path,$@)

commit 18bc2ed14408ef637a7f1eb8b87c9ce80f0a36a2

Fix missing libgcc.a when linking shared libraries.
This is required to ensure that the generated shared library / binary
includes all libgcc symbols it depends on.

Fixed initially in d7ea04d2f4b44b72732ecadbb14ae0e866cd5465, issue
described in detail in log message of
3c41e4cd7efddf2600ed6c2bc2091e140f0095f4, broken since
7a06c124be094134d81920a8f9fe363f6f30abd1 (ndk-r7c).

build/core/build-binary.mk
@@ -293,7 +293,8 @@ endif
 # The list of object/static/shared libraries passed to the linker when
 # building shared libraries and executables. order is important.
 #
-linker_objects_and_libraries := $(strip $(call TARGET-get-linker-objects-and-libraries,\
+# Cannot use immediate evaluation because PRIVATE_LIBGCC may not be defined at this point.
+linker_objects_and_libraries = $(strip $(call TARGET-get-linker-objects-and-libraries,\
     $(LOCAL_OBJECTS), \
     $(static_libraries), \
     $(whole_static_libraries), \