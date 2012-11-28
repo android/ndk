c05658a96c6caedbbb75fa5a41703c69b8776f0a
Andrew Hsieh <andrewhsieh@google.com>
2012-11-28 13:36:27 +0800

    Change bugURL to http://source.android.com/source/report-bugs.html
    See http://gcc.gnu.org/bugzilla/show_bug.cgi?id=55349#c3
    
    Change-Id: I76d47f66cfdaeb2ad780de810db7232694678d1a

--- build/tools/build-gcc.sh
+++ build/tools/build-gcc.sh
@@ -249,6 +249,7 @@ $BUILD_SRCDIR/configure --target=$ABI_CONFIGURE_TARGET \
                         --with-gcc-version=$GCC_VERSION \
                         --with-gdb-version=$GDB_VERSION \
                         --with-gxx-include-dir=$TOOLCHAIN_BUILD_PREFIX/include/c++/$GCC_VERSION \
+                        --with-bugurl=$DEFAULT_ISSUE_TRACKER_URL \
                         $EXTRA_CONFIG_FLAGS \
                         $ABI_CONFIGURE_EXTRA_FLAGS
 if [ $? != 0 ] ; then
