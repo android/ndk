5a242664149701e00be6eef18f93a5fe0635d949
Andrew Hsieh <andrewhsieh@google.com>
2012-11-30 17:49:01 +0800

    Add testcase for issue 20862
    See http://code.google.com/p/android/issues/detail?id=20862
    
    Change-Id: I3c2f41e7f50e8e6fd6aba5962bab15879ff8964c

---
+++ tests/build/issue20862-libpng-O0/jni/Android.mk
@@ -0,0 +1,16 @@
+LOCAL_PATH:= $(call my-dir)
+
+
+include $(CLEAR_VARS)
+
+LOCAL_ALLOW_UNDEFINED_SYMBOLS := true
+
+LOCAL_MODULE:= png
+
+LOCAL_CFLAGS := -I$(LOCAL_PATH)
+
+LOCAL_SRC_FILES := $(notdir $(filter-out %/example.c, $(wildcard $(LOCAL_PATH)/*.c)))
+
+LOCAL_LDLIBS :=
+
+include $(BUILD_SHARED_LIBRARY)
---
+++ tests/build/issue20862-libpng-O0/jni/Application.mk
@@ -0,0 +1,2 @@
+APP_ABI := all
+APP_CFLAGS := -O0 -g
