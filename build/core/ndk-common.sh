9f18ee5396934e597594f7191ebd42ed103d01b6
Andrew Hsieh <andrewhsieh@google.com>
2012-12-06 15:01:01 +0800

    Fix unpack_archive() to remove ._* files
    by MacOSX to preserve resource forks we don't need
    
    Change-Id: I1facfdcb34779515b74e7dd65a7970f3c689affd

--- build/core/ndk-common.sh
+++ build/core/ndk-common.sh
@@ -272,6 +272,7 @@ case "$HOST_ARCH" in
     # reporting anomoly here.
     if [ "$HOST_OS" = darwin ] ; then
         if ! echo __LP64__ | (CCOPTS= gcc -E - 2>/dev/null) | grep -q __LP64__ ; then
+        # or if gcc -dM -E - < /dev/null | grep -q __LP64__; then
             HOST_ARCH=x86_64
         fi
     fi
@@ -708,6 +709,8 @@ unpack_archive ()
             else
                 run tar j$TARFLAGS "$ARCHIVE" -C $DIR
             fi
+            # remove ._* files by MacOSX to preserve resource forks we don't need
+            find $DIR -name "\._*" -exec rm {} \;
             ;;
         *)
             panic "Cannot unpack archive with unknown extension: $ARCHIVE"
