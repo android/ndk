c5607fc90267eb396c4dd44e2754202d12748723
Andrew Hsieh <andrewhsieh@google.com>
2012-12-11 15:22:40 +0800

    Add testcase for MIPS FP madd/msub/nmadd/nmsub/recip/rsqrt
    See 7609f724df8ca390935f63243fa72e1de39d00c6
    
    Change-Id: I544b54c882e83fbe06337b23cb30c2cd3c0a77ac

---
+++ tests/build/mips-fp4/build.sh
@@ -0,0 +1,25 @@
+cd $(dirname "$0")
+
+$NDK/ndk-build -B "$@"
+
+# Find instruction in file
+# $1: instruction
+# $2: file
+find_instruction ()
+{
+    local INST=$1
+    local FILE=$2
+
+    grep -q -w -F -e $INST $FILE
+    if [ $? != 0 ]; then
+        echo "$INST expected in file $FILE"
+        exit 1
+    fi
+}
+
+find_instruction "madd.d" mips-fp4-test1-2.s
+find_instruction "msub.d" mips-fp4-test1-2.s
+find_instruction "nmadd.d" mips-fp4-test3-6.s
+find_instruction "nmsub.d" mips-fp4-test3-6.s
+find_instruction "recip.d" mips-fp4-test3-6.s
+find_instruction "rsqrt.d" mips-fp4-test3-6.s
