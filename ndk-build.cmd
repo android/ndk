18f80b1624fdf4491c674986564f07e486e06c75
Andrew Hsieh <andrewhsieh@google.com>
2013-01-03 20:59:07 +0800

    Fix ndk-build and ndk-build.cmd to observe NDK_HOST_32BIT
    Also allow NDK_LOG=0 to disable logging, and NDK_HOST_32BIT=0
    to disable force-32bit.
    
    Change-Id: I58d87d5ca1fe73eb33c88fee75418849641d2c5a

--- ndk-build.cmd
+++ ndk-build.cmd
@@ -14,8 +14,13 @@ goto :L
 call :FOO %NDK_ROOT%
 if ERRORLEVEL 1 exit /b 1
 
-rem Check if 64-bit make exists and Windows is also 64-bit
-if "%ProgramW6432%"=="" set NDK_MAKE=%NDK_ROOT%\prebuilt\windows\bin\make.exe
-if not exist %NDK_MAKE% set NDK_MAKE=%NDK_ROOT%\prebuilt\windows\bin\make.exe
+rem Check if NDK_HOST_32BIT is not set to 1/true, Windows is 64-bit, and 64-bit make exists
+if "%NDK_HOST_32BIT%"=="1" set NDK_MAKE=
+if "%NDK_HOST_32BIT%"=="true" set NDK_MAKE=
+if not exist %NDK_MAKE% set NDK_MAKE=
+if "%ProgramW6432%"=="" set NDK_MAKE=
+
+rem Otherwise fall back to 32-bit make
+if "%NDK_MAKE%"=="" set NDK_MAKE=%NDK_ROOT%\prebuilt\windows\bin\make.exe
 
 %NDK_MAKE% -f %NDK_ROOT%build/core/build-local.mk SHELL=cmd %*
