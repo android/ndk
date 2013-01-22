54ece731d8192427c41fdfe8a28a74c285de4452
David 'Digit' Turner <digit@android.com>
2013-01-22 12:00:30 +0100

    libc++: Add locale-specific wrappers for C library functions.
    This patch adds trivial stubs to implement extended C library functions
    like strcoll_l() by calling the respective non-locale specific function
    (e.g. strcoll() in this example).
    
    On Android, only the C/POSIX locale is supported. It might be useful
    in the future to improve the implementation by allowing it to use
    the platform APIs to implement all locale-specific operations properly.
    
    However, this requires a JavaVM handle that cannot be accessed from
    generic native code (the only way to get it is really through the
    JNI_OnLoad() hook when loading a shared library with Dalvik).
    
    This can be solved by doing some sort of lazy initialization, i.e.:
    
      1/ At first, only support the usual C/POSIX locale.
    
      2/ Provide a function like init_android_vm(JavaVM* vm) that can
         be called by client code to enable better locales.
    
    Still a lot of work, so for the future.
    
    Change-Id: I84268edaf9c3d4ad0d4ee237e270cbe1f0b6d58f

--- sources/cxx-stl/llvm-libc++/include/support/android/string.h
+++ sources/cxx-stl/llvm-libc++/include/support/android/string.h
@@ -4,11 +4,15 @@
 #include_next <string.h>
 #include <xlocale.h>
 
+#ifdef __cplusplus
 extern "C" {
+#endif
 
 int strcoll_l(const char*, const char*, locale_t);
 int strxfrm_l(char*, const char*, size_t, locale_t);
 
+#ifdef __cplusplus
 }  // extern "C"
+#endif
 
 #endif  // LLVM_LIBCXX_SUPPORT_ANDROID_STRING_H
--- sources/cxx-stl/llvm-libc++/include/support/android/time.h
+++ sources/cxx-stl/llvm-libc++/include/support/android/time.h
@@ -4,11 +4,15 @@
 #include_next <time.h>
 #include <xlocale.h>
 
+#ifdef __cplusplus
 extern "C" {
+#endif
 
 size_t strftime_l(char *s, size_t maxsize, const char *format,
                   const struct tm * timeptr, locale_t locale);
 
+#ifdef __cplusplus
 }  // extern "C"
+#endif
 
 #endif  // LLVM_LIBCXX_SUPPORT_ANDROID_TIME_H
