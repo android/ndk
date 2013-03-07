8bb75147eb405f552afe8297cf4fbbadfd5843ef
Andrew Hsieh <andrewhsieh@google.com>
2013-03-07 20:29:33 +0800

    Add more openmp tests
    Change-Id: Id2984bba0d117711ecf3398f79598a5d1a3b3d82

--- tests/device/test-openmp/jni/Android.mk
+++ tests/device/test-openmp/jni/Android.mk
@@ -7,3 +7,17 @@ LOCAL_CFLAGS += -fopenmp
 LOCAL_LDFLAGS += -fopenmp
 include $(BUILD_EXECUTABLE)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := openmp2
+LOCAL_SRC_FILES := openmp2.c
+LOCAL_CFLAGS += -fopenmp
+LOCAL_LDFLAGS += -fopenmp
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := fib
+LOCAL_SRC_FILES := fib.c
+LOCAL_CFLAGS += -fopenmp
+LOCAL_LDFLAGS += -fopenmp
+include $(BUILD_EXECUTABLE)
+
