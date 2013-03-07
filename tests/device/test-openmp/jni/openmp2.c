05c9d7bff3feb6be2b5cfb9a83caa612baf66104
Andrew Hsieh <andrewhsieh@google.com>
2013-03-07 20:29:33 +0800

    Add more openmp tests
    Change-Id: Id2984bba0d117711ecf3398f79598a5d1a3b3d82

---
+++ tests/device/test-openmp/jni/openmp2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#include <sys/sysconf.h>
+
+int main (int argc, char *argv[])
+{
+    int nthreads, tid;
+    printf("SC_NPROCESSORS_ONLN: %d\n", sysconf (_SC_NPROCESSORS_ONLN));
+  #pragma omp parallel default(shared) private(nthreads, tid)
+    /* Fork a team of threads giving them their own copies of variables */
+    {
+      /* Obtain thread number */
+        tid = omp_get_thread_num();
+        printf("Hello World from thread = %d\n", tid);
+      /* Only master thread does this */
+        if (tid == 0)
+        {
+            nthreads = omp_get_num_threads();
+            printf("Number of threads = %d\n", nthreads);
+        }
+    }  /* All threads join master thread and disband */
+
+  return 0;
+}
