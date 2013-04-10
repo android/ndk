006880c0dd68573f257393ba2363faf4aba25e4c
Lai Wei-Chih <Robert.Lai@mediatek.com>
2013-04-10 21:04:14 +0800

    Add bc2native.
    Change-Id: I69463c9d039cceca59d829e2a63f6934ace9bda6

--- toolchains/llvm-3.2/setup.mk
+++ toolchains/llvm-3.2/setup.mk
@@ -29,9 +29,10 @@ SYSROOT_LINK     := $(NDK_PLATFORMS_ROOT)/$(TARGET_PLATFORM)/arch-arm
 TARGET_GDBSERVER := $(NDK_ROOT)/prebuilt/android-arm/gdbserver/gdbserver
 TARGET_ARCH_ABI  := armeabi-v7a
 NDK_APP_DST_DIR  := $(NDK_APP_PROJECT_PATH)/libs/$(TARGET_ARCH_ABI)
-TARGET_LDLIBS    := $(NDK_ROOT)/sources/android/libportable/libs/armeabi-v7a/libportable.a $(TARGET_LDLIBS)
-TARGET_LDFLAGS   += -Wl,@$(NDK_ROOT)/sources/android/libportable/libs/armeabi-v7a/libportable.wrap
-include $(NDK_ROOT)/toolchains/arm-linux-androideabi-clang3.2/setup.mk
+cmd-strip = $(PRIVATE_STRIP) --strip-unneeded $(call host-path,$1) && \
+            $(NDK_ROOT)/ndk-bc2native-py --abi=$(TARGET_ARCH_ABI) \
+            --platform=$(TARGET_PLATFORM) \
+            --file $(call host-path,$1) $(patsubst %.bc,%.so,$(call host-path,$1))
 
 else
 ifneq ($(filter %armeabi,$(TARGET_ARCH_ABI)),)
@@ -40,9 +41,10 @@ SYSROOT_LINK     := $(NDK_PLATFORMS_ROOT)/$(TARGET_PLATFORM)/arch-arm
 TARGET_GDBSERVER := $(NDK_ROOT)/prebuilt/android-arm/gdbserver/gdbserver
 TARGET_ARCH_ABI  := armeabi
 NDK_APP_DST_DIR  := $(NDK_APP_PROJECT_PATH)/libs/$(TARGET_ARCH_ABI)
-TARGET_LDLIBS    := $(NDK_ROOT)/sources/android/libportable/libs/armeabi/libportable.a $(TARGET_LDLIBS)
-TARGET_LDFLAGS   += -Wl,@$(NDK_ROOT)/sources/android/libportable/libs/armeabi/libportable.wrap
-include $(NDK_ROOT)/toolchains/arm-linux-androideabi-clang3.2/setup.mk
+cmd-strip = $(PRIVATE_STRIP) --strip-unneeded $(call host-path,$1) && \
+            $(NDK_ROOT)/ndk-bc2native-py --abi=$(TARGET_ARCH_ABI) \
+            --platform=$(TARGET_PLATFORM) \
+            --file $(call host-path,$1) $(patsubst %.bc,%.so,$(call host-path,$1))
 
 else
 ifneq ($(filter %x86,$(TARGET_ARCH_ABI)),)
@@ -51,9 +53,10 @@ SYSROOT_LINK     := $(NDK_PLATFORMS_ROOT)/$(TARGET_PLATFORM)/arch-x86
 TARGET_GDBSERVER := $(NDK_ROOT)/prebuilt/android-x86/gdbserver/gdbserver
 TARGET_ARCH_ABI  := x86
 NDK_APP_DST_DIR  := $(NDK_APP_PROJECT_PATH)/libs/$(TARGET_ARCH_ABI)
-TARGET_LDLIBS    := $(NDK_ROOT)/sources/android/libportable/libs/x86/libportable.a $(TARGET_LDLIBS)
-TARGET_LDFLAGS   += -Wl,@$(NDK_ROOT)/sources/android/libportable/libs/x86/libportable.wrap
-include $(NDK_ROOT)/toolchains/x86-clang3.2/setup.mk
+cmd-strip = $(PRIVATE_STRIP) --strip-unneeded $(call host-path,$1) && \
+            $(NDK_ROOT)/ndk-bc2native-py --abi=$(TARGET_ARCH_ABI) \
+            --platform=$(TARGET_PLATFORM) \
+            --file $(call host-path,$1) $(patsubst %.bc,%.so,$(call host-path,$1))
 
 else
 ifneq ($(filter %mips,$(TARGET_ARCH_ABI)),)
@@ -62,11 +65,14 @@ SYSROOT_LINK     := $(NDK_PLATFORMS_ROOT)/$(TARGET_PLATFORM)/arch-mips
 TARGET_GDBSERVER := $(NDK_ROOT)/prebuilt/android-mips/gdbserver/gdbserver
 TARGET_ARCH_ABI  := mips
 NDK_APP_DST_DIR  := $(NDK_APP_PROJECT_PATH)/libs/$(TARGET_ARCH_ABI)
-TARGET_LDLIBS    := $(NDK_ROOT)/sources/android/libportable/libs/mips/libportable.a $(TARGET_LDLIBS)
-TARGET_LDFLAGS   += -Wl,@$(NDK_ROOT)/sources/android/libportable/libs/mips/libportable.wrap
-include $(NDK_ROOT)/toolchains/mipsel-linux-android-clang3.2/setup.mk
-
-else
+cmd-strip = $(PRIVATE_STRIP) --strip-unneeded $(call host-path,$1) && \
+            $(NDK_ROOT)/ndk-bc2native-py --abi=$(TARGET_ARCH_ABI) \
+            --platform=$(TARGET_PLATFORM) \
+            --file $(call host-path,$1) $(patsubst %.bc,%.so,$(call host-path,$1))
+endif
+endif
+endif
+endif
 
 TOOLCHAIN_NAME   := clang-3.2
 TOOLCHAIN_PREFIX := $(TOOLCHAIN_PREBUILT_ROOT)/bin/
@@ -127,7 +133,3 @@ $(call set-src-files-target-cflags, $(__debug_sources), $(TARGET_debug_CFLAGS)) 
 $(call set-src-files-target-cflags, $(__release_sources),$(TARGET_release_CFLAGS)) \
 $(call set-src-files-text,$(LOCAL_SRC_FILES),plus$(space)$(space)) \
 
-endif
-endif
-endif
-endif
