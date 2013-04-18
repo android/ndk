fca9020881ee88259c2f0f1731107f833a0741a8
Andrew Hsieh <andrewhsieh@google.com>
2013-04-18 14:56:34 +0800

    Fix two test errors:
    1. -static is a linker flag
    2. fix chrono test since clang now sides with gcc-4.7
    
    Change-Id: Ieb2b0c626caa2675a76f8124e47d676bba08d671

--- tests/device/issue38121/jni/Android.mk
+++ tests/device/issue38121/jni/Android.mk
@@ -3,5 +3,6 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := issue38121_main
 LOCAL_SRC_FILES := main.c
-LOCAL_CFLAGS += -fstack-protector-all -static
+LOCAL_CFLAGS += -fstack-protector-all
+LOCAL_LDFLAGS += -static
 include $(BUILD_EXECUTABLE)
