dd8fc70f5479bb634c0919eed7f906d540ec807b
Andrew Hsieh <andrewhsieh@google.com>
2013-04-22 12:57:19 +0800

    Misc fixes
    1) Recent commit incorrectly build GCC4.4.3/mips with binutils-2.19.
       Change it back to compile with binutils-2.21
    2) ndk-stack used to be built only in 32-bit due to concern of
       overwriting 32-bit with 64-bit one in the same path.  The way
       NDK is currently packaged (ie. separate 32-bit and 64-bit)
       obliterates the concern.
    3) Fix python mingw 32-bit build and package name
    4) Fix gen-toolchain-wrapper.sh to not put null.o in $NDK directory
       while testing for presence of dlfcn.h
    
    Change-Id: I83aaee1310faad114f241086f3488d5842fc3e1d

--- build/tools/build-host-prebuilts.sh
+++ build/tools/build-host-prebuilts.sh
@@ -251,14 +251,9 @@ for SYSTEM in $SYSTEMS; do
     fi
 
     # First, ndk-stack
-    if [ "$TRY64" != "yes" ]; then
-        # Don't build ndk-stack in 64-bit because unlike other host toolchains
-        # ndk-stack doesn't have separate directories for 32-bit and 64-bit.
-        # 64-bit one will overwrite the 32-bit one
-        echo "Building $SYSNAME ndk-stack"
-        run $BUILDTOOLS/build-ndk-stack.sh $TOOLCHAIN_FLAGS
-        fail_panic "ndk-stack build failure!"
-    fi
+    echo "Building $SYSNAME ndk-stack"
+    run $BUILDTOOLS/build-ndk-stack.sh $TOOLCHAIN_FLAGS
+    fail_panic "ndk-stack build failure!"
 
     echo "Building $SYSNAME ndk-depends"
     run $BUILDTOOLS/build-ndk-depends.sh $TOOLCHAIN_FLAGS
