735d57d5d1c055db9e0ef28857e8dc7b3d7d6b9b
David 'Digit' Turner <digit@android.com>
2013-04-24 14:18:45 +0200

    Add GoogleTest to the NDK.
    This patch adds the following:
    
    - sources/third_party/googletest/googletest/:
      The sources for the latest GoogleTest revision.
    
    - sources/third_party/googletest/patches.ndk/:
      The patches applied to the upstream sources.
    
    - sources/third_party/googletest/README.NDK:
      Readme file explaining what this is + basic usage instructions.
    
    - sources/third_party/googletest/Android.mk:
      Build file declaring the modules to be imported to use GoogleTest
    
    - tests/device/test-googletest-stlport:
      Simple test to build GoogleTest against STLport and run a few
      sample programs.
    
    - tests/device/test-googletest-gnustl:
      Same as above, but linking against GNU libstdc++.
      This is required because GoogleTest will compile differently
      based on the features of the C++ STL being used (e.g. STLport
      doesn't have <tr1/tuple>)
    
    - tests/device/test-googletest-full:
      The full GoogleTest unit test program (740 tests). Ignored by
      tests/run-tests.sh by default since it's long to build and run.
    
    + Update .gitignore to only ignore the top-level 'samples' directory.
    
    Change-Id: I6ed5bc55d16f513daa8e237096c3f6771c2d9772

---
+++ tests/device/test-googletest-full/jni/Application.mk
@@ -0,0 +1,4 @@
+APP_ABI := all
+APP_MODULES := googletest_unittest
+APP_STL := stlport_static
+APP_PLATFORM := android-9
---
+++ tests/device/test-googletest-stlport/jni/Application.mk
@@ -0,0 +1,14 @@
+APP_ABI := all
+
+APP_MODULES := \
+    googletest_sample_1 \
+    googletest_sample_2 \
+    googletest_sample_3 \
+    googletest_sample_4 \
+    googletest_sample_5 \
+    googletest_sample_6 \
+    googletest_sample_7 \
+    googletest_sample_8 \
+    googletest_sample_9 \
+
+APP_STL := stlport_shared
