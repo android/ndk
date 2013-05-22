c39351a729f274b844387658eddc63085d6913ff
Ray Donnelly <mingw.android@gmail.com>
2013-05-22 11:44:01 +0100

    Use Python 2.7.5

--- build/tools/download-toolchain-sources.sh
+++ build/tools/download-toolchain-sources.sh
@@ -216,7 +216,7 @@ toolchain_checkout "" $BRANCH expat .
 toolchain_checkout "" $BRANCH binutils binutils-2.19 binutils-2.21 binutils-2.22 binutils-2.23
 toolchain_checkout "" $BRANCH gcc gcc-4.4.3 gcc-4.6 gcc-4.7 gcc-4.8
 toolchain_checkout "" $BRANCH gdb gdb-6.6 gdb-7.3.x
-toolchain_checkout "" $BRANCH python Python-2.7.4
+toolchain_checkout "" $BRANCH python Python-2.7.5
 toolchain_checkout "" $BRANCH perl perl-5.16.2
 toolchain_checkout "" $BRANCH mclinker .
 
