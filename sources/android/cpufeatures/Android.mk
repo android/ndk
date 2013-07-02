931dd55dac1900f1959ab8fc8b186ba51409a5e6
Lai Wei-Chih <Robert.Lai@mediatek.com>
2013-07-02 15:09:01 +0800

    Add cpufeatures library into libportable.
    cpufeatures library is target dependent, and we should not
    compile it into bitcode. This is the reason why we fail on
    tests/device/test-cpufeatures.
    
    For unknown arch, we should use the one in libportable.
    
    Change-Id: I7c56f39389c8101e0fc2e73babac5ed3f6312406

--- sources/android/cpufeatures/Android.mk
+++ sources/android/cpufeatures/Android.mk
@@ -1,5 +1,24 @@
 LOCAL_PATH := $(call my-dir)
 
+ifneq ($(strip $(filter-out $(NDK_KNOWN_ARCHS),$(TARGET_ARCH))),)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := cpufeatures
+LOCAL_SRC_FILES := $(NDK_ROOT)/sources/android/libportable/libs/$(TARGET_ARCH_ABI)/libportable.a
+
+ifdef HISTORICAL_NDK_VERSIONS_ROOT
+# This is included by the platform build system.
+LOCAL_SDK_VERSION := 9
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)
+else
+# This is included by the NDK build system.
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
+endif
+
+include $(PREBUILT_STATIC_LIBRARY)
+
+else
+
 include $(CLEAR_VARS)
 LOCAL_MODULE := cpufeatures
 LOCAL_SRC_FILES := cpu-features.c
@@ -14,3 +33,5 @@ LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
 endif
 
 include $(BUILD_STATIC_LIBRARY)
+
+endif
