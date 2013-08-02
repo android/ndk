c81d48f03cad675c8c5ae9e063f02612304c064a
Andrew Hsieh <andrewhsieh@google.com>
2013-08-02 12:13:43 +0800

    Simplify finding windows host name to workaround a bug in WINE
    Previously find-ndk-root-win-host.cmd set both env. vars: NDK_ROOT
    and NDK_WIN_HOST with the following compound statement
    
        endlocal && set "NDK_ROOT=%TEMP1%" && set "NDK_WIN_HOST=%TEMP2%"
    
    Unfortunately, the "cmd" in WINE appends " " to all but the last
    "set", causing error message reads:
    
      Can't recognize '"Z:\tmp\android-ndk-r9\prebuilt/windows-x86_64 /bin/make.exe" -f
          "Z:\tmp\android-ndk-r9\build/core/build-local.mk" SHELL=cmd
          as an internal or external command, or batch script.
    
    Rename to find-win-host.cmd and recompute NDK_ROOT in both callers
    
    Change-Id: I1c22ca905db74a09b3b99c06d70ef89b66732397

--- ndk-build.cmd
+++ ndk-build.cmd
@@ -1,5 +1,6 @@
 @echo off
 rem This is a Windows cmd.exe script used to invoke the NDK-specific GNU Make executable
-call "%~dp0find-ndk-root-win-host.cmd" NDK_ROOT NDK_WIN_HOST
-if %ERRORLEVEL% neq 0 (exit /b 1)
+call "%~dp0find-win-host.cmd" NDK_WIN_HOST
+if ERRORLEVEL 1 (exit /b 1)
+set NDK_ROOT=%~dp0
 "%NDK_ROOT%prebuilt/%NDK_WIN_HOST%/bin/make.exe" -f "%NDK_ROOT%build/core/build-local.mk" SHELL=cmd %*
