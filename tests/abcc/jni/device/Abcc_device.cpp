271a09c18f6973e63925483be047a4e34075c261
WenHan Gu <Wenhan.gu@mediatek.com>
2013-10-01 13:28:55 +0800

    Integrate abcc codebase.
    This CL needs https://android-review.googlesource.com/#/c/66545/
    
    Integrated abcc will be used after llvm 3.3 (included) by default.
    If we want to use python version, we can add --use-python to build-llvm.sh.
    
    Every host-related will be put under <abcc>/host/.
    Every device-related will be put under <abcc>/device/.
    Other common-based will be put under <abcc>.
    
    The usage is just the same like before. No learning curve issue.
    On host, we can dump information if we use --verbose.
    On device, we can -DVERBOSE=1 to dump info to logcat.
    
    Change-Id: Ib95a3aabb40dc85bf73f8a35254108688ac654b7

---
+++ tests/abcc/jni/device/Abcc_device.cpp
@@ -0,0 +1,218 @@
+/*
+ * Copyright 2013, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <dirent.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sstream>
+#include <sys/stat.h>
+#if ENABLE_PARALLEL_LLVM_CG
+#include <cpu-features.h>
+#endif
+#include "Abcc_device.h"
+using namespace abcc;
+
+#if defined(__arm__) && defined(__ARM_ARCH_7A__)
+# define CURRENT_ABI  "armeabi-v7a"
+#elif defined(__arm__)
+# define CURRENT_ABI  "armeabi"
+#elif defined(__i386__)
+# define CURRENT_ABI  "x86"
+#elif defined(__mips__)
+# define CURRENT_ABI  "mips"
+#else
+# error "Unsupport target abi"
+#endif
+
+
+DeviceBitcodeCompiler::DeviceBitcodeCompiler(const std::string &working_dir, const std::string &sysroot)
+  : BitcodeCompiler(CURRENT_ABI, sysroot, working_dir) {}
+
+void DeviceBitcodeCompiler::cleanupPost() {
+  std::string cmd("echo ");
+  if (mRet == RET_OK)
+    cmd += "0";
+  else
+    cmd += "255";
+  cmd += " > " + mWorkingDir + "/compile_result";
+  runCmd(cmd);
+
+  cmd = "chmod 0644 " + mWorkingDir + "/compile_result";
+  runCmd(cmd);
+
+  // TODO: Dump whole flow if failed
+  if (mRet != RET_OK) {
+    cmd = "touch " + mWorkingDir + "/compile_error";
+    runCmd(cmd);
+    cmd = "chmod 0644 " + mWorkingDir + "/compile_error";
+    runCmd(cmd);
+  }
+}
+
+int DeviceBitcodeCompiler::parseLDFlags(BitcodeInfo &info, const std::string &orig_str) {
+  std::stringstream ss(orig_str);
+  std::string str;
+  while (ss >> str) {
+    if (str.find("--sysroot") != std::string::npos) {
+      continue;
+    }
+    if (str == "-o") {
+      ss >> str;
+      continue;
+    }
+    if (str == "-soname") {
+      ss >> str;
+      info.mSOName = str;
+      continue;
+    }
+    if (str == "-shared") {
+      info.mShared = true;
+      info.mOutPath += ".so";
+      continue;
+    }
+
+    // Parse -lxxx
+    if (str.size() > 2 &&
+        str.substr(0, 2) == "-l") {
+      info.mLDLibs.push_back(str.substr(2));
+      info.mLDLibsStr += " " + str;
+      continue;
+    }
+
+    // Some other flags, like --no-undefined, -z now, -z noexecstack, ...
+    info.mLDFlags += str + " ";
+  } // while
+  return 0;
+}
+
+void DeviceBitcodeCompiler::getBitcodeFiles() {
+  std::vector<std::string> files;
+  DIR *dp = opendir(mWorkingDir.c_str());
+  if (!dp) {
+    mRet = RET_FAIL_PREPARE_BITCODE;
+    return;
+  }
+
+  struct dirent *entry = 0;
+  while ((entry = readdir(dp)) != 0) {
+    std::string filename(entry->d_name);
+    if (filename == "." || filename == "..")
+      continue;
+
+    std::string full_path = mWorkingDir + "/" + filename;
+    int fd = open(full_path.c_str(), O_RDONLY);
+    if (fd < 0) {
+      LOGV("Error opening file: %s (Ignored)", full_path.c_str());
+      continue;
+    }
+
+    unsigned char buffer[4];
+    read(fd, buffer, 4);
+    close(fd);
+    BitcodeInfo::swapEndian(buffer, 4);
+    int magic = BitcodeInfo::transferBytesToNum(buffer, 4);
+
+    if (magic != 0x0b17c0de) {
+      LOGV("Found file %s magic: %x, but we need a wrapped bitcode.", full_path.c_str(), magic);
+      continue;
+    }
+
+    LOGV("Push_back a bitcode: %s", full_path.c_str());
+    files.push_back(full_path);
+  } // while
+  closedir(dp);
+
+  if (files.empty()) {
+    LOGV("No bitcodes needs to compile");
+    return;
+  }
+
+  if (returnCode() != RET_OK) {
+    LOGV("Cannot get bitcode files from directory: %s", mWorkingDir.c_str());
+    return;
+  }
+
+  for (std::vector<std::string>::const_iterator i = files.begin(),
+       e = files.end(); i != e; ++i) {
+    BitcodeInfo bcinfo(*i);
+    if (bcinfo.readWrapper(*this) != 0) {
+      LOGE("Cannot read wrapper for bitcode %s", i->c_str());
+      mRet = RET_FAIL_PREPARE_BITCODE;
+      return;
+    }
+    mBitcodeFiles.push_back(bcinfo);
+  }
+}
+
+void DeviceBitcodeCompiler::prepareToolchain() {
+  // le32-none-ndk-translate
+  std::string cmd = std::string("LD_LIBRARY_PATH=") + mSysroot + "/usr/lib";
+  cmd += " " + mSysroot + "/usr/bin/le32-none-ndk-translate";
+  mExecutableToolsPath[(unsigned)CMD_TRANSLATE] = cmd;
+
+  // llc
+  cmd = std::string("LD_LIBRARY_PATH=") + mSysroot + "/usr/lib";
+  cmd += " " + mSysroot + "/usr/bin/llc";
+#ifdef ENABLE_PARALLEL_LLVM_CG
+  std::ostringstream stream;
+  stream << android_getCpuCount();
+  cmd += " -thread=" + stream.str();
+#endif
+  mExecutableToolsPath[(unsigned)CMD_COMPILE] = cmd;
+
+  // ld.mcld
+  cmd = std::string("LD_LIBRARY_PATH=") + mSysroot + "/usr/lib";
+  cmd += " " + mSysroot + "/usr/bin/ld.mcld";
+  cmd += " -L" + mWorkingDir;
+  cmd += " -L" + mSysroot + "/usr/lib";
+  cmd += " -L/system/lib";
+  mExecutableToolsPath[(unsigned)CMD_LINK] = cmd;
+
+  cmd = mSysroot + "/usr/lib/libcompiler_rt_static.a";
+  cmd += " " + mSysroot + "/usr/lib/libgabi++_shared.so";
+  cmd += " @" + mSysroot + "/usr/lib/libportable.wrap " + mSysroot + "/usr/lib/libportable.a";
+  cmd += " -ldl";
+  mExecutableToolsPath[(unsigned)CMD_LINK_RUNTIME] = cmd;
+}
+
+void DeviceBitcodeCompiler::copyRuntime(const BitcodeInfo &info) {
+  runCmd(std::string("cp -f ") + mSysroot + "/usr/lib/libgabi++_shared.so " + mWorkingDir + "/libgabi++_shared.so");
+  runCmd(std::string("chmod 0755 ") + mWorkingDir + "/libgabi++_shared.so");
+
+  std::stringstream ss(info.mLDLibsStr);
+  std::string deplib;
+  while (ss >> deplib) {
+    if (deplib.substr(0, 2) == "-l") {
+      std::string libname = "lib" + deplib.substr(2) + ".so";
+      std::string libpath = mSysroot + "/usr/lib/" + libname;
+      struct stat buf;
+      if (stat(libpath.c_str(), &buf) == 0) {
+        // Found!
+        runCmd("cp -f " + libpath + " " + mWorkingDir + "/" + libname);
+      }
+    }
+  }
+}
+
+void DeviceBitcodeCompiler::removeIntermediateFile(const std::string &path) {
+#if VERBOSE
+  ReturnCode code = mRet;
+  runCmd(std::string("chmod 0755 ") + path);  // Fix permission for future use
+  mRet = code;  // Ignore error when change mode
+#else
+  runCmd(std::string("rm -f ") + path);
+#endif
+}
---
+++ tests/abcc/jni/host/Abcc_host.cpp
@@ -0,0 +1,153 @@
+/*
+ * Copyright 2013, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <sstream>
+#include "Abcc_host.h"
+using namespace abcc;
+
+bool kVerbose = false;
+
+HostBitcodeCompiler::HostBitcodeCompiler(const std::string &abi, const std::string &sysroot,
+                                         const std::string &input, const std::string &output, const std::string &working_dir,
+                                         const std::string &platform)
+  : BitcodeCompiler(abi, sysroot, working_dir), mIn(input), mOut(output),
+    mNDKDir(""), mPlatform(platform) {
+}
+
+HostBitcodeCompiler::HostBitcodeCompiler(const std::string &abi, const std::string &sysroot, const std::string &ndk_dir, const std::string &toolchain_bin,
+                                         const std::string &input, const std::string &output, const std::string &working_dir,
+                                         const std::string &platform)
+  : BitcodeCompiler(abi, sysroot, working_dir), mIn(input), mOut(output),
+    mNDKDir(ndk_dir), mPlatform(platform), mToolchainBinPath(toolchain_bin) {
+}
+
+int HostBitcodeCompiler::parseLDFlags(BitcodeInfo &info, const std::string &orig_str) {
+  std::stringstream ss(orig_str);
+  std::string str;
+  while (ss >> str) {
+    if (str.find("--sysroot") != std::string::npos) {
+      continue;
+    }
+    if (str == "-o") {
+      ss >> str;
+      continue;
+    }
+    if (str == "-soname") {
+      ss >> str;
+      info.mSOName = str;
+      continue;
+    }
+    if (str == "-shared") {
+      info.mShared = true;
+      info.mOutPath += ".so";
+      continue;
+    }
+
+    if (str.size() > 2 &&
+        str.substr(str.size() - 2) == ".a") {
+      info.mLDLibs.push_back(str);
+      info.mLDLibsStr += " " + str;
+      continue;
+    }
+    if (str.size() > 3 &&
+        str.substr(str.size() - 3) == ".so") {
+      info.mLDLibs.push_back(str);
+      info.mLDLibsStr += " " + str;
+      continue;
+    }
+
+    // Parse -lxxx
+    if (str.size() > 2 &&
+        str.substr(0, 2) == "-l") {
+      info.mLDLibs.push_back(str.substr(2));
+      info.mLDLibsStr += " " + str;
+      continue;
+    }
+
+    // Some other flags, like --no-undefined, -z now, -z noexecstack, ...
+    info.mLDFlags += str + " ";
+  } // while
+  return 0;
+}
+
+void HostBitcodeCompiler::getBitcodeFiles() {
+  BitcodeInfo bcinfo(mIn);
+  if (bcinfo.readWrapper(*this) != 0) {
+    LOGE("Cannot read wrapper for bitcode %s", mIn.c_str());
+    mRet = RET_FAIL_PREPARE_BITCODE;
+    return;
+  }
+
+  bcinfo.mOutPath = mOut;
+  mBitcodeFiles.push_back(bcinfo);
+}
+
+void HostBitcodeCompiler::prepareToolchain() {
+  // le32-none-ndk-translate
+  std::string cmd = getToolchainBinPath() + "/le32-none-ndk-translate";
+  mExecutableToolsPath[(unsigned)CMD_TRANSLATE] = cmd;
+
+  // llc
+  cmd = getToolchainBinPath() + "/llc";
+  mExecutableToolsPath[(unsigned)CMD_COMPILE] = cmd;
+
+  // ld.mcld
+  cmd = getToolchainBinPath() + "/ld.mcld";
+  cmd += " -L" + mSysroot + "/usr/lib";
+  mExecutableToolsPath[(unsigned)CMD_LINK] = cmd;
+
+  cmd = getCompilerRTPath() + "/libcompiler_rt_static.a";
+  cmd += " " + getGAbixxPath() + "/libgabi++_shared.so";
+  cmd += " @" + getLibPortablePath() + "/libportable.wrap " + getLibPortablePath() + "/libportable.a";
+  cmd += " -ldl";
+  mExecutableToolsPath[(unsigned)CMD_LINK_RUNTIME] = cmd;
+}
+
+void HostBitcodeCompiler::copyRuntime(const BitcodeInfo &info) {
+}
+
+void HostBitcodeCompiler::removeIntermediateFile(const std::string &path) {
+  if (!kVerbose)
+    runCmd(std::string("rm -f ") + path);
+}
+
+const std::string HostBitcodeCompiler::getToolchainBinPath() const {
+  if (!mNDKDir.empty())
+    return mToolchainBinPath;
+  else
+    return mSysroot + "/usr/bin";
+}
+
+const std::string HostBitcodeCompiler::getCompilerRTPath() const {
+  if (!mNDKDir.empty())
+    return std::string(mNDKDir) + "/sources/android/compiler-rt/libs/" + (const char*)mAbi;
+  else
+    return mSysroot + "/usr/lib";
+}
+
+const std::string HostBitcodeCompiler::getGAbixxPath() const {
+  if (!mNDKDir.empty())
+    return std::string(mNDKDir) + "/sources/cxx-stl/gabi++/libs/" + (const char*)mAbi;
+  else
+    return mSysroot + "/usr/lib";
+}
+
+const std::string HostBitcodeCompiler::getLibPortablePath() const {
+  if (!mNDKDir.empty())
+    return std::string(mNDKDir) + "/sources/android/libportable/libs/" + (const char*)mAbi;
+  else
+    return mSysroot + "/usr/lib";
+}
