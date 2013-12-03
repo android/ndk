da0d4464de8c1038cf06c935a9f5aac9661a4c99
Andrew Hsieh <andrewhsieh@google.com>
2013-12-03 08:16:14 +0800

    Build static libs with Deterministic (option D) mode of ar tool
    See https://code.google.com/p/android/issues/detail?id=60705 and
    b2f39181c6998661cd1146d77baa892d60e3d6c3
    
    Change-Id: Ibeac3fcfceb56ef6723a68297249e35b6f0821f0

--- build/tools/builder-funcs.sh
+++ build/tools/builder-funcs.sh
@@ -318,7 +318,7 @@ builder_static_library ()
     fi
     builder_log "${_BUILD_PREFIX}Archive: $libname"
     rm -f "$lib"
-    builder_command ${_BUILD_AR} crs "$lib" "$_BUILD_OBJECTS"
+    builder_command ${_BUILD_AR} crsD "$lib" "$_BUILD_OBJECTS"
     fail_panic "Could not archive ${_BUILD_PREFIX}$libname objects!"
 }
 
@@ -340,7 +340,7 @@ builder_host_static_library ()
     fi
     builder_log "${_BUILD_PREFIX}Archive: $libname"
     rm -f "$lib"
-    builder_command ${_BUILD_AR} crs "$lib" "$_BUILD_OBJECTS"
+    builder_command ${_BUILD_AR} crsD "$lib" "$_BUILD_OBJECTS"
     fail_panic "Could not archive ${_BUILD_PREFIX}$libname objects!"
 }
 
