57e74362be4f7ce589f51fe1f7c2993db3c9920b
David 'Digit' Turner <digit@android.com>
2013-12-09 11:48:57 +0100

    Fix 'test-wait' test.
    wait4() is only available since API level 19, before that the
    C library didn't export the symbol. See [1] for the
    corresponding NDK platforms patch.
    
    + Rewrite the test to avoid sleeping and too much copy-and-paste.
    
    + Add a test-wait-api19 test that links the test program against
      API level 19 system libraries, which include wait4().
    
    http://b.android.com/19854
    
    [1] https://android-review.googlesource.com/#/c/71850/
    
    Change-Id: Ia79461082a23b615650263102645ef302c14653f

---
+++ tests/device/test-wait-api19/jni/Application.mk
@@ -0,0 +1,5 @@
+APP_ABI := all
+APP_PLATFORM := android-19
+# When building the static executable, avoids the linker error message:
+# 'fatal error: -pie and -static are incompatible'
+APP_PIE := false
