943360bc58fde652f8d8c4ed3dd9decccc92f27e
Andrew Hsieh <andrewhsieh@google.com>
2014-01-22 18:21:18 -0800

    Support APP_ABI=armeabi-v7a-hard
    Add a new NDK-only ABI armeabi-v7a-hard in order to solve
    -mfloat-abi=hard issue.
    
    1. Note that this is NDK-only ABI.  No known devices
       support armeabi-v7a-hard, and they can't w/o breaking existing float-abi=softfp
       code
    2. APP_ABI=armeabi-v7a-hard still installs libraries in libs/armeabi-v7a.
       As a result, armeabi-v7a-hard and armeabi-v7a can't co-exist ("make"
       will ignore one of the "install"), unless when _NDK_TESTING_ALL_=yes for
       internal testing (where lib is installed to libs/armeabi-v7a-hard)
    3. Both GCC 4.6/4.8 and Clang3.3/3.4 has additional multilib
       option "-mfloat-abi=hard" to look for libraries in /hard
       sub-directory.
         5be615df32ce970fcccda93cf577e9ce35b1d397 (gcc4.6,4.8)
         7e8798057dc5f28b44b5608fd98fff1cba6909a2 (clang3.4)
         deedd8557b7d82c6931b6942bd39a00127780835 (clang3.3)
    4. APP_ABI=all still expand to armeabi,armeabi-v7a,x86,mips
    
    Change-Id: I08c26bfa6580c80260185c2fef398848aebd197d

--- toolchains/arm-linux-androideabi-4.8/setup.mk
+++ toolchains/arm-linux-androideabi-4.8/setup.mk
@@ -35,13 +35,20 @@ TARGET_LDFLAGS := -no-canonical-prefixes
 TARGET_C_INCLUDES := \
     $(SYSROOT_INC)/usr/include
 
-ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
+ifneq ($(filter $(TARGET_ARCH_ABI), armeabi-v7a armeabi-v7a-hard),)
     TARGET_CFLAGS += -march=armv7-a \
-                     -mfloat-abi=softfp \
                      -mfpu=vfpv3-d16
-
     TARGET_LDFLAGS += -march=armv7-a \
                      -Wl,--fix-cortex-a8
+ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
+    TARGET_CFLAGS += -mfloat-abi=softfp
+else
+    TARGET_CFLAGS += -mhard-float \
+                     -D_NDK_MATH_NO_SOFTFP=1
+    TARGET_LDFLAGS += -Wl,--no-warn-mismatch \
+                     -lm_hard
+endif
+
 else
     TARGET_CFLAGS += -march=armv5te \
                             -mtune=xscale \
