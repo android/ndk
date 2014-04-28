0c4514c3403fb57f96c956251a064d9e1f0a11ca
Andrew Hsieh <andrewhsieh@google.com>
2014-04-27 18:07:41 -0700

    Build non-thumb version of STL gabi++/stlport/libc++
    Previously only thumb version of gabi++/stlport/libc++ are built,
    and place it at the sources/cxx-stl/*libs/<arch> directories,
    instead of /thumb directory directly below.  This CL build
    both arm and thumb versions of library, and put thumb version in /thumb
    
    Note that gnu-libstdc++ already contains /thumb, but ndk-build never uses it.
    Now ndk-build system use non-thumb as before, unless LOCAL_ARM_MODE=arm
    
    Change-Id: I48e25d0f1b578929e8d5a32bb6c8d434c79c8a91

--- sources/cxx-stl/gnu-libstdc++/Android.mk
+++ sources/cxx-stl/gnu-libstdc++/Android.mk
@@ -17,6 +17,12 @@ gnustl_exported_c_includes := \
 include $(CLEAR_VARS)
 LOCAL_MODULE := gnustl_static
 LOCAL_SRC_FILES := $(TOOLCHAIN_VERSION)/libs/$(TARGET_ARCH_ABI)/libgnustl_static$(TARGET_LIB_EXTENSION)
+# For armeabi*, choose thumb mode unless LOCAL_ARM_MODE := arm
+ifneq (,$(filter armeabi%,$(TARGET_ARCH_ABI)))
+ifneq (arm,$(LOCAL_ARM_MODE))
+LOCAL_SRC_FILES:= $(TOOLCHAIN_VERSION)/libs/$(TARGET_ARCH_ABI)/thumb/libgnustl_static$(TARGET_LIB_EXTENSION)
+endif
+endif
 LOCAL_EXPORT_CPPFLAGS := $(gnustl_exported_cppflags)
 LOCAL_EXPORT_C_INCLUDES := $(gnustl_exported_c_includes)
 include $(PREBUILT_STATIC_LIBRARY)
@@ -24,6 +30,12 @@ include $(PREBUILT_STATIC_LIBRARY)
 include $(CLEAR_VARS)
 LOCAL_MODULE := gnustl_shared
 LOCAL_SRC_FILES := $(TOOLCHAIN_VERSION)/libs/$(TARGET_ARCH_ABI)/libgnustl_shared$(TARGET_SONAME_EXTENSION)
+# For armeabi*, choose thumb mode unless LOCAL_ARM_MODE := arm
+ifneq (,$(filter armeabi%,$(TARGET_ARCH_ABI)))
+ifneq (arm,$(LOCAL_ARM_MODE))
+LOCAL_SRC_FILES:= $(TOOLCHAIN_VERSION)/libs/$(TARGET_ARCH_ABI)/thumb/libgnustl_shared$(TARGET_SONAME_EXTENSION)
+endif
+endif
 LOCAL_EXPORT_CPPFLAGS := $(gnustl_exported_cppflags)
 LOCAL_EXPORT_C_INCLUDES := $(gnustl_exported_c_includes)
 LOCAL_EXPORT_LDLIBS := $(call host-path,$(LOCAL_PATH)/$(TOOLCHAIN_VERSION)/libs/$(TARGET_ARCH_ABI)/libsupc++$(TARGET_LIB_EXTENSION))
