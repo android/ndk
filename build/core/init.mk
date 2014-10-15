e8b7056ecd6c3d0549cae9c7620862e5d07c2e37
Andrew Hsieh <andrewhsieh@google.com>
2014-10-15 15:32:11 +0800

    Rename android-L as android-21
    1. Now that we back to numeric API level, some hack to treat 'L'
       specially are removed.
    2. Translate android-L to android-21 for now to ease transition
    3. Error on the existance of stale platforms/android-L
    
    Change-Id: Ie2ee22096122903d7bce638992d2027140f6e5e4

--- build/core/init.mk
+++ build/core/init.mk
@@ -491,17 +491,17 @@ $(foreach _platform,$(NDK_ALL_PLATFORMS),\
 NDK_PREVIEW_LEVEL := L
 NDK_ALL_PLATFORM_LEVELS := $(filter android-%,$(NDK_ALL_PLATFORMS))
 NDK_ALL_PLATFORM_LEVELS := $(patsubst android-%,%,$(NDK_ALL_PLATFORM_LEVELS))
+ifneq (,$(filter $(NDK_PREVIEW_LEVEL),$(NDK_ALL_PLATFORM_LEVELS)))
+    $(call __ndk_info,Please remove stale preview platforms/android-$(NDK_PREVIEW_LEVEL))
+    $(call __ndk_info,API level android-L is renamed as android-21.)
+    $(call __ndk_error,Aborting)
+endif
 $(call ndk_log,Found stable platform levels: $(NDK_ALL_PLATFORM_LEVELS))
 
-# Hack to pull $(NDK_PREVIEW_LEVEL) ahead of all (numeric) level
 NDK_MAX_PLATFORM_LEVEL := 3
-_max_theoretical_api_level := 99
 $(foreach level,$(NDK_ALL_PLATFORM_LEVELS),\
-  $(eval NDK_MAX_PLATFORM_LEVEL := $$(if $$(subst $$(NDK_PREVIEW_LEVEL),,$$(level)),$$(call max,$$(NDK_MAX_PLATFORM_LEVEL),$$(level)),$(_max_theoretical_api_level)))\
+  $(eval NDK_MAX_PLATFORM_LEVEL := $$(call max,$$(NDK_MAX_PLATFORM_LEVEL),$$(level)))\
 )
-ifeq ($(NDK_MAX_PLATFORM_LEVEL),$(_max_theoretical_api_level))
-NDK_MAX_PLATFORM_LEVEL := $(NDK_PREVIEW_LEVEL)
-endif
 
 $(call ndk_log,Found max platform level: $(NDK_MAX_PLATFORM_LEVEL))
 
