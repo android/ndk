2fb6451352d24f1843fc7c828d0d125a11e60eb0
Dan Albert <danalbert@google.com>
2015-06-19 01:13:00 -0700

    Put build output in DIST_DIR if set.
    Change-Id: I77b07e1ee59beb98ce6f8d2cbae440609650fa91

--- checkbuild.sh
+++ checkbuild.sh
@@ -3,5 +3,14 @@ set -e
 
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
+PACKAGE_DIR_ARG=
+if [ -n "$DIST_DIR" ]; then
+    PACKAGE_DIR_ARG="--package-dir=$(./realpath $DIST_DIR)"
+fi
+
 build/tools/dev-cleanup.sh
-build/tools/rebuild-all-prebuilt.sh $(./realpath ../toolchain) --verbose $*
+build/tools/rebuild-all-prebuilt.sh \
+    $(./realpath ../toolchain) \
+    --verbose \
+    $PACKAGE_DIR_ARG \
+    $*
