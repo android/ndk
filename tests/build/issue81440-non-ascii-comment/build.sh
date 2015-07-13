2a0fa0514bec3d087847a5172585cda26177781d
Dan Albert <danalbert@google.com>
2015-07-13 16:48:49 -0700

    Don't assume clang crashed; print the output.
    Clang didn't crash, I just hadn't exported $NDK.
    
    Change-Id: Icd4e9e94ceb0635dc3e25c69a198e93954c474cc

--- tests/build/issue81440-non-ascii-comment/build.sh
+++ tests/build/issue81440-non-ascii-comment/build.sh
@@ -2,12 +2,15 @@
 # See b.android.com/81440
 #
 
-NDK_TOOLCHAIN_VERSION=clang $NDK/ndk-build -B APP_ABI=armeabi 2>&1 | grep -q "error: expected"
+result=$(NDK_TOOLCHAIN_VERSION=clang $NDK/ndk-build -B APP_ABI=armeabi 2>&1)
+echo $result | grep -q "error: expected"
 RET=$?
 rm -rf obj
 
 if [ $RET != 0 ]; then
-  echo "Error: clang crashes"
+  echo "Error: did not find 'error: expected'"
+  echo "Output:"
+  echo $result
   exit 1
 fi
 
