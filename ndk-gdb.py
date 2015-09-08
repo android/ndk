29a9b48139483e6cbaa30d6b68d9eeb1ab54f655
Josh Gao <jmgao@google.com>
2015-09-08 13:42:41 -0700

    [ndk-gdb.py] Select which ps command to run on the device.
    Saves an adb roundtrip.
    
    Bug: http://b/23793246
    Change-Id: Iffce325f7576102cdbf8d2ca7530c77c049b1503

--- ndk-gdb.py
+++ ndk-gdb.py
@@ -451,13 +451,17 @@ def get_pid_of(package_name):
     package names like com.exampleisverylongtoolongbyfar.plasma
     exceed the limit.
     '''
-    ps_command = 'ps'
-    retcode,output = adb_cmd(False, ['shell', 'readlink $(which ps)'])
-    if output:
-        output = output.replace('\r', '').splitlines()[0]
-    if output == 'busybox':
-        ps_command = 'ps -w'
-    retcode,output = adb_cmd(False,['shell', ps_command])
+
+    # Perform the check on the device to avoid an adb roundtrip
+    ps_script = '''
+        if [ $(readlink $(which ps)) == "toolbox" ]; then
+            ps;
+        else
+            ps -w;
+        fi
+    '''
+    ps_script = " ".join([line.strip() for line in ps_script.splitlines()])
+    retcode, output = adb_cmd(False, ['shell', ps_script])
     output = output.replace('\r', '').splitlines()
     columns = output.pop(0).split()
     try:
