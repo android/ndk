b73f03465cd817edcfc8da7c4c972eb19777f96e
Nikola Veljkovic <Nikola.Veljkovic@imgtec.com>
2015-09-14 19:22:34 +0200

    test-cpufeatures: Add support for remaining architectures
    cpufeatures has support for all architectures.
    Update the test accordingly.
    
    Change-Id: I953bd9461b0263392a444b9de698cb98695f5f68

--- tests/device/test-cpufeatures/jni/test_cpufeatures.c
+++ tests/device/test-cpufeatures/jni/test_cpufeatures.c
@@ -46,26 +46,64 @@ int main(void)
         return 1;
     }
 
-    if (family == ANDROID_CPU_FAMILY_ARM) {
-        uint64_t features = android_getCpuFeatures();
+#define CHECK(arch, name) \
+    if ((features & ANDROID_CPU_## arch ##_FEATURE_## name) != 0) { \
+        printf( "  "#name"\n" ); \
+    }
+
+    uint64_t features = android_getCpuFeatures();
+    int result = 0;
+    switch(family) {
+    case ANDROID_CPU_FAMILY_ARM:
         printf( "Supported ARM features:\n");
-#define CHECK(name) \
-        if ((features & ANDROID_CPU_ARM_FEATURE_## name) != 0) { \
-            printf( "  "#name"\n" ); \
+        CHECK(ARM, ARMv7)
+        CHECK(ARM, VFPv3)
+        CHECK(ARM, NEON)
+        CHECK(ARM, LDREX_STREX)
+        CHECK(ARM, VFPv2)
+        CHECK(ARM, VFP_D32)
+        CHECK(ARM, VFP_FP16)
+        CHECK(ARM, VFP_FMA)
+        CHECK(ARM, NEON_FMA)
+        CHECK(ARM, IDIV_ARM)
+        CHECK(ARM, IDIV_THUMB2)
+        CHECK(ARM, iWMMXt)
+        CHECK(ARM, AES)
+        CHECK(ARM, PMULL)
+        CHECK(ARM, SHA1)
+        CHECK(ARM, SHA2)
+        CHECK(ARM, CRC32)
+        break;
+    case ANDROID_CPU_FAMILY_ARM64:
+        CHECK(ARM64, FP)
+        CHECK(ARM64, ASIMD)
+        CHECK(ARM64, AES)
+        CHECK(ARM64, PMULL)
+        CHECK(ARM64, SHA1)
+        CHECK(ARM64, SHA2)
+        CHECK(ARM64, CRC32)
+        break;
+    case ANDROID_CPU_FAMILY_X86:
+    case ANDROID_CPU_FAMILY_X86_64:
+        printf( "Supported x86 features:\n");
+        CHECK(X86, SSSE3)
+        CHECK(X86, POPCNT)
+        CHECK(X86, MOVBE)
+        CHECK(X86, SSE4_1)
+        CHECK(X86, SSE4_2)
+        break;
+    case ANDROID_CPU_FAMILY_MIPS:
+    case ANDROID_CPU_FAMILY_MIPS64:
+        printf( "Supported MIPS features:\n");
+        CHECK(MIPS, R6)
+        CHECK(MIPS, MSA)
+        break;
+    default:
+        if (features != 0) {
+            printf("ERROR: Unexpected CPU features mask: %016" PRIX64 "\n",
+                   features);
+            result = 1;
         }
-        CHECK(LDREX_STREX)
-        CHECK(VFPv2)
-        CHECK(ARMv7)
-        CHECK(VFPv3)
-        CHECK(VFP_D32)
-        CHECK(VFP_FP16)
-        CHECK(VFP_FMA)
-        CHECK(NEON)
-        CHECK(NEON_FMA)
-        CHECK(IDIV_ARM)
-        CHECK(IDIV_THUMB2)
-        CHECK(iWMMXt)
-#undef CHECK
     }
 
 #ifdef __arm__
@@ -76,33 +114,6 @@ int main(void)
     printf( "   part       : %03x\n", (cpu_id >> 4) & 0xfff);
     printf( "   revision   : %x\n",    cpu_id & 0x0f);
 #endif
-
-    if (family == ANDROID_CPU_FAMILY_X86) {
-        uint64_t features = android_getCpuFeatures();
-        printf( "Supported x86 features:\n");
-#define CHECK(name) \
-        if ((features & ANDROID_CPU_X86_FEATURE_## name) != 0) { \
-            printf( "  "#name"\n" ); \
-        }
-        CHECK(SSSE3)
-        CHECK(POPCNT)
-        CHECK(MOVBE)
-#undef CHECK
-    }
-
-    int result = 0;
-
-    // Check that the CPU features mask is empty for anything that isn't
-    // 32-bit ARM or 32-bit x86.
-    if (family != ANDROID_CPU_FAMILY_ARM &&
-        family != ANDROID_CPU_FAMILY_X86) {
-        uint64_t features = android_getCpuFeatures();
-        if (features != 0) {
-            printf("ERROR: Unexpected CPU features mask: %016" PRIX64 "\n",
-                   features);
-            result = 1;
-        }
-    }
 
     int count = android_getCpuCount();
     printf( "Number of CPU cores: %d\n", count);
