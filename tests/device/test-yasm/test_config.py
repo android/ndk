74d686ca5d906175f2ae56b23b461a00443fbe2b
Dan Albert <danalbert@google.com>
2015-10-02 16:39:17 -0700

    Mark test-yasm broken for x86_64.
    Change-Id: I5f9afe9ae0aa719b8e3d0e9bfb7a55b0ac270075

--- tests/device/test-yasm/test_config.py
+++ tests/device/test-yasm/test_config.py
@@ -2,3 +2,8 @@ def match_unsupported(abi, platform, toolchain, subtest=None):
     if abi not in ('x86', 'x86_64'):
         return abi
     return None
+
+def match_broken(abi, platform, toolchain, subtest=None):
+    if abi == 'x86_64':
+        return abi, 'http://b/24620865'
+    return None, None
