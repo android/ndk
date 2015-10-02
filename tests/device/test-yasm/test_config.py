32b0749ab0407127e5e16a4a748f9fa6815590f4
Dan Albert <danalbert@google.com>
2015-10-02 16:39:17 -0700

    Mark test-yasm unsupported for x86_64.
    This is testing 32-bit asm. We might want to add another test for
    x86_64 later.
    
    Change-Id: I5f9afe9ae0aa719b8e3d0e9bfb7a55b0ac270075

--- tests/device/test-yasm/test_config.py
+++ tests/device/test-yasm/test_config.py
@@ -2,3 +2,7 @@ def match_unsupported(abi, platform, toolchain, subtest=None):
     if abi not in ('x86', 'x86_64'):
         return abi
     return None
+
+def match_broken(abi, platform, toolchain, subtest=None):
+    if abi == 'x86_64':
+        return abi, 'http://b/24620865'
