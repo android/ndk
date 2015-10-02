b5defb4df863a90a3678456cccefe14a67744050
Dan Albert <danalbert@google.com>
2015-10-02 16:27:00 -0700

    Mark android support tests as broken for all LP64.
    Bug: http://b/24614583
    Change-Id: I1ba782143236206de6484e5fdbe06b5de0cc1ff3

--- tests/device/test-android-support/test_config.py
+++ tests/device/test-android-support/test_config.py
@@ -1,4 +1,5 @@
 def match_broken(abi, platform, toolchain, subtest=None):
-    if subtest == 'android_support_unittests' and abi == 'arm64-v8a':
+    lp64_abis = ('arm64-v8a', 'mips64', 'x86_64')
+    if subtest == 'android_support_unittests' and abi in lp64_abis:
         return abi, 'http://b/24614583'
     return None, None
