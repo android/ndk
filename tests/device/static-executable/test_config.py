97d6a7d2f6b6b3ed90d8f9ab7647f67af15a5c28
Dan Albert <danalbert@google.com>
2015-10-02 16:33:38 -0700

    Fix static-executable test failure check.
    These are failing because of the android-21 libc.a, not because of the
    ABI.
    
    Bug: http://b/24468267
    Change-Id: I58ed124b5470ab5cc74f0679179ca8d1259c6f0d

--- tests/device/static-executable/test_config.py
+++ tests/device/static-executable/test_config.py
@@ -1,4 +1,12 @@
 def match_broken(abi, platform, toolchain, subtest=None):
-    if abi == 'arm64-v8a':
-        return abi, 'http://b/24468267'
+    lp64_abis = ('arm64-v8a', 'mips64', 'x86_64')
+    if platform is not None:
+        platform_version = int(platform.split('-')[1])
+        if platform_version >= 21:
+            return platform, 'http://b/24468267'
+    elif abi in lp64_abis:
+        # If platform isn't provided we're going to use the default platform
+        # level. For 32-bit ABIs the default is old enough that we'll pass, but
+        # 64-bit ABIs will fail.
+        return 'android-21', 'http://b/24468267'
     return None, None
