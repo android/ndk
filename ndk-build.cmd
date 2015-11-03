fc08327e0407517d2a7614a55a1f0aec2c38e93b
Dan Albert <danalbert@google.com>
2015-11-03 23:05:22 +0000

    Revert "Revert "Remove host tag from path in prebuilt/.""
    Previous issue was that I had forgotten to upload
    https://android-review.googlesource.com/#/c/180253/
    
    This reverts commit 6a3516c0d91396e062915019c1b7959f359d49b4.
    
    Change-Id: I7734954c84f7eb4343a5e74984c419717eb53fa6

--- ndk-build.cmd
+++ ndk-build.cmd
@@ -1,6 +1,3 @@
 @echo off
-rem This is a Windows cmd.exe script used to invoke the NDK-specific GNU Make executable
-call "%~dp0find-win-host.cmd" NDK_WIN_HOST
-if ERRORLEVEL 1 (exit /b 1)
 set NDK_ROOT=%~dp0
-"%NDK_ROOT%prebuilt/%NDK_WIN_HOST%/bin/make.exe" -f "%NDK_ROOT%build/core/build-local.mk" SHELL=cmd %*
+"%NDK_ROOT%prebuilt/bin/make.exe" -f "%NDK_ROOT%build/core/build-local.mk" SHELL=cmd %*
--- ndk-gdb-py.cmd
+++ ndk-gdb-py.cmd
@@ -1,9 +1,6 @@
 @echo off
-rem This is a Windows cmd.exe script used to invoke the NDK-specific Python executable
-call "%~dp0find-win-host.cmd" NDK_WIN_HOST
-if ERRORLEVEL 1 (exit /b 1)
 setlocal
 set NDK_ROOT=%~dp0
 set SHELL=cmd
-"%NDK_ROOT%prebuilt/%NDK_WIN_HOST%/bin/python.exe" -u "%~dp0ndk-gdb.py" %*
+"%NDK_ROOT%prebuilt/bin/python.exe" -u "%~dp0ndk-gdb.py" %*
 endlocal
