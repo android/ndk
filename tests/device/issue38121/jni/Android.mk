dd7ec3c871a0cc704438e5f47588709cc9ee6869
Dan Albert <danalbert@google.com>
2015-12-02 16:22:28 -0800

    Remove test for x86 gcc stack protector hack.
    We used to have a hack in GCC to protect against the referenced issue
    (http://b.android.com/38121), but we have since removed that
    (https://android-review.googlesource.com/#/c/182731/).
    
    This test was really checking the behavior of the compiler, not libc
    (see the -static). We already test that we don't have this problem as
    part of the bionic tests, so it adds no value here.
    
    Change-Id: If59fb4d28475995473953ac915b1e60254456164

--- tests/device/issue38121/jni/Android.mk
+++
@@ -1,8 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := issue38121_main
-LOCAL_SRC_FILES := main.c
-LOCAL_CFLAGS += -fstack-protector-all
-LOCAL_LDFLAGS += -static -Wl,--eh-frame-hdr
-include $(BUILD_EXECUTABLE)
