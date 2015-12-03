e861e58a443a7a083378227c6d1a1e47b5ea4b1d
Dan Albert <danalbert@google.com>
2015-12-03 11:11:01 -0800

    Make a test more deterministic, mark as broken.
    We removed the BROKEN_BUILD tag from this quite some time ago because
    we were always testing with APP_PIE=true. The problem tested does not
    appear when using -fPIE, so add -static to prevent that. Now that the
    problem is always visible, mark it as broken again.
    
    Change-Id: I7f1f602768c0d70a5e34d15c340757b43605b36f

--- tests/build/issue65705-asm-pc/test_config.py
+++ tests/build/issue65705-asm-pc/test_config.py
@@ -1,3 +1,9 @@
+def match_broken(abi, device, toolchain, subtest=None):
+    if toolchain == '4.9':
+        return toolchain, 'http://b.android.com/65705'
+    return None, None
+
+
 def match_unsupported(abi, device, toolchain, subtest=None):
     if abi not in ('armeabi', 'armeabi-v7a', 'armeabi-v7a-hard'):
         return abi
