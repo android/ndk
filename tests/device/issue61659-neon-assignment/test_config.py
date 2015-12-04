f66a2b30699da68beff278eabc4944e7e085fd79
Dan Albert <danalbert@google.com>
2015-12-03 17:47:08 -0800

    Disable broken test for Ginger Bread.
    Bug: http://b/26016050
    Change-Id: I2a7fc921aa70edac1f012635336991a6e8d38664

--- tests/device/issue61659-neon-assignment/test_config.py
+++ tests/device/issue61659-neon-assignment/test_config.py
@@ -1,3 +1,9 @@
+def match_broken(abi, platform, device_platform, toolchain, subtest=None):
+    if device_platform <= 10:
+        return device_platform, 'http://b/26016050'
+    return None
+
+
 def match_unsupported(abi, platform, device_platform, toolchain, subtest=None):
     if abi not in ('armeabi-v7a', 'armeabi-v7a-hard'):
         return abi
