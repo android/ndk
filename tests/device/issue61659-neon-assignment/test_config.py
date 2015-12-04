0148501f55d9ff0e713890824d434edf6aca6972
Dan Albert <danalbert@google.com>
2015-12-03 17:47:08 -0800

    Disable test for unsupported platforms.
    The test is built for android-18.
    
    Change-Id: I2a7fc921aa70edac1f012635336991a6e8d38664

--- tests/device/issue61659-neon-assignment/test_config.py
+++ tests/device/issue61659-neon-assignment/test_config.py
@@ -1,4 +1,6 @@
 def match_unsupported(abi, platform, device_platform, toolchain, subtest=None):
     if abi not in ('armeabi-v7a', 'armeabi-v7a-hard'):
         return abi
+    if device_platform < 18:
+        return device_platform
     return None
