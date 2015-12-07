95f1eabff0b5f0cc96529f2f11de5125c1854dd6
Egor Kochetov <egor.kochetov@intel.com>
2015-12-07 15:38:15 +0300

    Fixed the path to ndk-build in $NDK/tests/ndk.py
    Since 'ndk-build' has moved to '$NDK/build', we need this fix, otherwise
    'run-all.py' dies with the 'no such file' error.
    
    Change-Id: I2727653dcdb85c76ddb8daf00798f7327de8c709
    Signed-off-by: Egor Kochetov <egor.kochetov@intel.com>

--- tests/ndk.py
+++ tests/ndk.py
@@ -34,7 +34,7 @@ def get_tool(tool):
 
 
 def build(build_flags):
-    ndk_build_path = os.path.join(os.environ['NDK'], 'ndk-build')
+    ndk_build_path = os.path.join(os.environ['NDK'], 'build', 'ndk-build')
     if os.name == 'nt':
         ndk_build_path += '.cmd'
     return util.call_output([ndk_build_path] + build_flags)
