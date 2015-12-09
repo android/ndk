a478321e18ee2b5f98ad9cad086bc4ec47322747
Dan Albert <danalbert@google.com>
2015-12-08 16:39:06 -0800

    Remove some obsolete tests.
    The NDK used to support defining `_WCHAR_IS_8BIT` to get an 8-bit
    wchar (this was for compatibility with Froyo and earlier releases). We
    don't support targeting anything before Gingerbread any more, so I
    don't think we need this compatibility flag now (it's already gone
    from the NDK sys/cdefs.h).
    
    Change-Id: I13e748a9038b2c7c80d417e3ddb0c6bd9ae57bd5

--- tests/build/wchar_t-size/jni/Android.mk
+++ tests/build/wchar_t-size/jni/Android.mk
@@ -4,9 +4,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := test_wchar_t_size
 LOCAL_SRC_FILES := \
   test_default.c \
-  test_8bit.c \
-  test_8bit_cplusplus.cpp \
-  test_8bit_cplusplus_stdc_limit_macros.cpp \
   test_always_signed.c \
 
 include $(BUILD_SHARED_LIBRARY)
