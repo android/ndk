e16d8e0d194e06449f7cb8283266f5b3ca1d9c5d
Dan Albert <danalbert@google.com>
2016-01-21 13:03:46 -0800

    Remove the redundant binutils and gcclibs.
    Since we're not ready for the modular layout yet, we don't need to
    ship these components separately as they are already in the GCC
    package. Remove them to save some space.
    
    Change-Id: Idaa0f53dcbb98f99af8340365598cee409c42c9b
    (cherry picked from commit 35a7b70856064b3331c0d1323891b1065e814e1b)

--- build/core/toolchains/aarch64-linux-android-clang3.6/setup.mk
+++ build/core/toolchains/aarch64-linux-android-clang3.6/setup.mk
@@ -32,7 +32,8 @@ LLVM_TOOLCHAIN_PREFIX := $(LLVM_TOOLCHAIN_PREBUILT_ROOT)/bin/
 
 TOOLCHAIN_NAME := aarch64-linux-android
 BINUTILS_ROOT := $(call get-binutils-root,$(NDK_ROOT),$(TOOLCHAIN_NAME))
-TOOLCHAIN_PREFIX := $(BINUTILS_ROOT)/bin/$(TOOLCHAIN_NAME)-
+TOOLCHAIN_ROOT := $(call get-toolchain-root,$(NDK_ROOT),$(TOOLCHAIN_NAME)-4.9)
+TOOLCHAIN_PREFIX := $(TOOLCHAIN_ROOT)/bin/$(TOOLCHAIN_NAME)-
 
 TARGET_CC := $(LLVM_TOOLCHAIN_PREFIX)clang$(HOST_EXEEXT)
 TARGET_CXX := $(LLVM_TOOLCHAIN_PREFIX)clang++$(HOST_EXEEXT)
@@ -40,7 +41,7 @@ TARGET_CXX := $(LLVM_TOOLCHAIN_PREFIX)clang++$(HOST_EXEEXT)
 LLVM_TRIPLE := aarch64-none-linux-android
 
 TARGET_CFLAGS := \
-    -B $(call host-path,$(BINUTILS_ROOT))/$(TOOLCHAIN_NAME)/bin \
+    -gcc-toolchain $(call host-path,$(TOOLCHAIN_ROOT)) \
     -target $(LLVM_TRIPLE) \
     -ffunction-sections \
     -funwind-tables \
@@ -51,8 +52,7 @@ TARGET_CFLAGS := \
     -no-canonical-prefixes
 
 TARGET_LDFLAGS += \
-    -B $(call host-path,$(BINUTILS_ROOT))/$(TOOLCHAIN_NAME)/bin \
-    -L $(call get-gcclibs-path,$(NDK_ROOT),$(TOOLCHAIN_NAME)) \
+    -gcc-toolchain $(call host-path,$(TOOLCHAIN_ROOT)) \
     -target $(LLVM_TRIPLE) \
     -no-canonical-prefixes
 
