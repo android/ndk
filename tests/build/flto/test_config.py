e4a5382109c831e0cf440b6986b3b75ba6b63f99
Dan Albert <danalbert@google.com>
2016-02-08 17:00:35 -0800

    Mark tests as unsupported for non-Linux hosts.
    Change-Id: Ia623a31d8430603fab812ad689d2c06a4626c77d
    (cherry picked from commit 186246d457bed18809b2a6fc21f42796ea3ece9d)

--- tests/build/flto/test_config.py
+++ tests/build/flto/test_config.py
@@ -1,4 +1,15 @@
-def match_unsupported(abi, platform, toolchain, subtest=None):
+# Shut up a warning about us not being a real package.
+from __future__ import absolute_import
+import platform
+
+
+def match_unsupported(abi, _platform, toolchain, _subtest=None):
     # Clang does LTO via gold plugin, but gold doesn't support MIPS yet.
     if toolchain == 'clang' and abi.startswith('mips'):
         return '{} {}'.format(toolchain, abi)
+
+    # We only support LTO from Linux.
+    if platform.system() != 'Linux':
+        return platform.system()
+
+    return None
