705dd431d63649655041bcb16f7a5921d176509c
Dan Albert <danalbert@google.com>
2016-02-11 14:27:00 -0800

    Revert "Update clang to match the plaform."
    The platform's current clang has issues with GAS and -Os, which
    happens to be the default release configuration for ARM in the NDK.
    We'll have to wait for a clang fix before we can update.
    
    This reverts commit cb06ce19aae53b810e829bc827866738988591bc.
    
    Bug: http://b/27151698
    Change-Id: I77d497ea8c0d66e8279dbd3fa84c0cf65c5a5e53

--- build/tools/build-llvm.py
+++ build/tools/build-llvm.py
@@ -35,7 +35,7 @@ def get_llvm_prebuilt_path(host):
 
 
 def main(args):
-    LLVM_VERSION = 'clang-2577113'
+    LLVM_VERSION = 'clang-2481030'
 
     host = args.host
     package_dir = args.dist_dir
