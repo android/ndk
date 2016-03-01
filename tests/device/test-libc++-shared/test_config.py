0cd0aac7170867f0025531310448cb9b2fd20b32
Dan Albert <danalbert@google.com>
2016-02-29 16:46:44 -0800

    Remove a test broken annotation.
    I believe this was fixed when I made the changes to how we build
    libc++, but I don't know that for a fact. Works now though.
    
    Bug: http://b/26014163
    Change-Id: I9a6494a656cc3d3a887ed8a06c2020c013e4a79e

--- tests/device/test-libc++-shared/test_config.py
+++
@@ -1,4 +0,0 @@
-def match_broken(abi, platform, device_platform, toolchain, subtest=None):
-    if abi == 'armeabi-v7a-hard' and device_platform <= 16:
-        return '{} {}'.format(abi, device_platform), 'http://b/26014163'
-    return None, None
