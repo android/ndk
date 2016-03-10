c9b170dc9514ec9a65ef3eaf163ca2e7d32e2ef0
Josh Gao <jmgao@google.com>
2016-03-09 18:20:23 -0800

    ndk-gdb.py: fix bad paths.
    These weren't updated for the directory reshuffling a while back.
    
    Bug: https://github.com/android-ndk/ndk/issues/3
    Change-Id: Ifea9be5d910a1ea2d974eb67f8a079f227d03218

--- ndk-gdb.py
+++ ndk-gdb.py
@@ -32,8 +32,8 @@ import xml.etree.cElementTree as ElementTree
 import logging
 
 # Shared functions across gdbclient.py and ndk-gdb.py.
-# ndk-gdb is installed to $NDK/host-tools/bin
-NDK_PATH = os.path.normpath(os.path.join(os.path.dirname(__file__), '../..'))
+# ndk-gdb is installed to $NDK/prebuilt/<platform>/bin
+NDK_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..'))
 sys.path.append(os.path.join(NDK_PATH, "python-packages"))
 import gdbrunner
 
@@ -182,11 +182,7 @@ def extract_launchable(xmlroot):
 
 
 def ndk_bin_path():
-    path = os.path.join(NDK_PATH, "host-tools", "bin")
-    if not os.path.exists(path):
-        error("Failed to find ndk binary path, should be at '{}'".format(path))
-
-    return path
+    return os.path.dirname(os.path.realpath(__file__))
 
 
 def handle_args():
