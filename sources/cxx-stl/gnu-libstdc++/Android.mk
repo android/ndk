6b49d8dfb141b3bc83c3e124d34d7b8b0586f691
Courtney Goeltzenleuchter <courtneygo@google.com>
2016-03-16 13:39:16 -0600

    Workaround: Does this need to be submitted?
    Change-Id: Id3e39ef2ebc95f2f50ecd3e842057e7121b99f63

--- sources/cxx-stl/gnu-libstdc++/Android.mk
+++ sources/cxx-stl/gnu-libstdc++/Android.mk
@@ -1,5 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
+GNUSTL_PREFIX:=4.9/
 # Compute the compiler flags to export by the module.
 # This is controlled by the APP_GNUSTL_FORCE_CPP_FEATURES variable.
 # See docs/APPLICATION-MK.html for all details.
@@ -10,17 +11,17 @@ gnustl_exported_cppflags := $(strip \
 
 # Include path to export
 gnustl_exported_c_includes := \
-  $(LOCAL_PATH)/4.9/include \
-  $(LOCAL_PATH)/4.9/libs/$(TARGET_ARCH_ABI)/include \
-  $(LOCAL_PATH)/4.9/include/backward
+  $(LOCAL_PATH)/$(GNUSTL_PREFIX)include \
+  $(LOCAL_PATH)/$(GNUSTL_PREFIX)libs/$(TARGET_ARCH_ABI)/include \
+  $(LOCAL_PATH)/$(GNUSTL_PREFIX)include/backward
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := gnustl_static
-LOCAL_SRC_FILES := 4.9/libs/$(TARGET_ARCH_ABI)/libgnustl_static$(TARGET_LIB_EXTENSION)
+LOCAL_SRC_FILES := $(GNUSTL_PREFIX)libs/$(TARGET_ARCH_ABI)/libgnustl_static$(TARGET_LIB_EXTENSION)
 # For armeabi*, choose thumb mode unless LOCAL_ARM_MODE := arm
 ifneq (,$(filter armeabi%,$(TARGET_ARCH_ABI)))
 ifneq (arm,$(LOCAL_ARM_MODE))
-LOCAL_SRC_FILES:= 4.9/libs/$(TARGET_ARCH_ABI)/thumb/libgnustl_static$(TARGET_LIB_EXTENSION)
+LOCAL_SRC_FILES:= $(GNUSTL_PREFIX)libs/$(TARGET_ARCH_ABI)/thumb/libgnustl_static$(TARGET_LIB_EXTENSION)
 endif
 endif
 LOCAL_EXPORT_CPPFLAGS := $(gnustl_exported_cppflags)
@@ -29,14 +30,14 @@ include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := gnustl_shared
-LOCAL_SRC_FILES := 4.9/libs/$(TARGET_ARCH_ABI)/libgnustl_shared$(TARGET_SONAME_EXTENSION)
+LOCAL_SRC_FILES := $(GNUSTL_PREFIX)libs/$(TARGET_ARCH_ABI)/libgnustl_shared$(TARGET_SONAME_EXTENSION)
 # For armeabi*, choose thumb mode unless LOCAL_ARM_MODE := arm
 ifneq (,$(filter armeabi%,$(TARGET_ARCH_ABI)))
 ifneq (arm,$(LOCAL_ARM_MODE))
-LOCAL_SRC_FILES:= 4.9/libs/$(TARGET_ARCH_ABI)/thumb/libgnustl_shared$(TARGET_SONAME_EXTENSION)
+LOCAL_SRC_FILES:= $(GNUSTL_PREFIX)libs/$(TARGET_ARCH_ABI)/thumb/libgnustl_shared$(TARGET_SONAME_EXTENSION)
 endif
 endif
 LOCAL_EXPORT_CPPFLAGS := $(gnustl_exported_cppflags)
 LOCAL_EXPORT_C_INCLUDES := $(gnustl_exported_c_includes)
-LOCAL_EXPORT_LDLIBS := $(call host-path,$(LOCAL_PATH)/4.9/libs/$(TARGET_ARCH_ABI)/libsupc++$(TARGET_LIB_EXTENSION))
+LOCAL_EXPORT_LDLIBS := $(call host-path,$(LOCAL_PATH)/$(GNUSTL_PREFIX)libs/$(TARGET_ARCH_ABI)/libsupc++$(TARGET_LIB_EXTENSION))
 include $(PREBUILT_SHARED_LIBRARY)
