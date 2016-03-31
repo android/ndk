4f7a55fe39e2718220f82fc77a43061fd61cd526
Dan Albert <danalbert@google.com>
2016-03-31 10:44:13 -0700

    Correct test config for ifuncs.
    Bug: http://b/22699878
    Change-Id: I04a8c7cad1a07f6cb9fe9b0a75174233ea5d463f

--- tests/device/libcxx-relocs/test_config.py
+++ tests/device/libcxx-relocs/test_config.py
@@ -1,5 +1,6 @@
 def match_broken(abi, platform, device_platform, toolchain, subtest=None):
-    if abi == 'armeabi' and toolchain == '4.9':
+    # The ifunc only gets used on armeabi, by GCC. We support ifuncs for M+.
+    if abi == 'armeabi' and toolchain == '4.9' and device_platform < 23:
         bug = 'https://github.com/android-ndk/ndk/issues/31'
-        return toolchain + ' ' + abi, bug
+        return toolchain + ' ' + abi + ' ' + device_platform, bug
     return None, None
