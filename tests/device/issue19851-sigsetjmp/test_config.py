582c24e3843a4afbaf22e74036c411e589572628
Dan Albert <danalbert@google.com>
2016-04-14 15:28:19 -0700

    Correct a couple test configs.
    Using `match_broken` on a device test without checking the subtest
    means that the test *build* is broken. Right now there's no visible
    difference for tests that contain only a single test case, but I'm
    about to add support for building the device tests even if we don't
    run them, at which point we will need to get this right.
    
    Change-Id: I1cc2ceadccf4844b0f392510a7a7cd13f8115416

--- tests/device/issue19851-sigsetjmp/test_config.py
+++ tests/device/issue19851-sigsetjmp/test_config.py
@@ -1,4 +1,4 @@
 def match_broken(abi, platform, device_platform, toolchain, subtest=None):
-    if device_platform <= 10:
+    if device_platform <= 10 and subtest == 'issue19851-sigsetjmp':
         return device_platform, 'http://b/26015756'
     return None, None
